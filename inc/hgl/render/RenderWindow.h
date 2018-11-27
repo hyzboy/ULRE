067761139271396e08643290b432d3114ef8697c
hyzboy <hyzboy@gmail.com>
2018-11-27 21:22:49 +0800

    增加RenderWindow定义和GLFW简单实现

---
+++ inc/hgl/render/RenderWindow.h
@@ -0,0 +1,51 @@
+﻿#ifndef HGL_RENDER_WINDOW_INCLUDE
+#define HGL_RENDER_WINDOW_INCLUDE
+
+#include<hgl/type/Vertex2.h>
+#include<hgl/type/BaseString.h>
+namespace hgl
+{
+    /**
+    * 渲染窗口基类
+    */
+    class RenderWindow
+    {
+    protected:
+
+        OSString caption;
+        bool full_screen;
+
+        int width,height;
+
+    public:
+
+        const uint      GetWidth()const{return width;}
+        const uint      GetHeight()const{return height;}
+        const bool      GetFullScreen()const{return full_screen;}
+
+    public:	//方法
+
+        RenderWindow()=default;                                                                     ///<本类构造函数
+        virtual ~RenderWindow()=default;                                                            ///<本类析构函数
+
+        virtual void ToMin()=0;																        ///<窗口最小化
+        virtual void ToMax()=0;																        ///<窗口最大化
+
+        virtual void Show()=0;																		///<显示窗口
+        virtual void Hide()=0;																		///<隐藏窗口
+
+        virtual const OSString &GetCaption()const{return caption;}
+        virtual void SetCaption(const OSString &)=0;
+
+    public:	//被实际操作系统接口层所调用的函数，在不了解的情况下请不要使用
+
+        virtual void SetSize(int w,int h)=0;														///<设置窗口大小
+
+        virtual void MakeToCurrent()=0;																///<切换到当前
+        virtual void SwapBuffer()=0;																///<交换缓冲区
+        virtual void WaitEvent(const double &time_out=0)=0;                                         ///<等待下一个事件
+        virtual void PollEvent()=0;                                                                 ///<轮询事件
+        virtual bool IsOpen()=0;                                                                    ///<是否依然存在
+    };//class RenderWindow
+}//namespace hgl
+#endif//HGL_RENDER_WINDOW_INCLUDE
---
+++ src/RenderDevice/GLFW/RenderWindowGLFW.cpp
@@ -0,0 +1,69 @@
+﻿#include<hgl/render/RenderWindow.h>
+#include<GLFW/glfw3.h>
+
+namespace hgl
+{
+    class RenderWindowGLFW:public RenderWindow
+    {
+        GLFWwindow *glfw_win;
+
+    public:
+
+        RenderWindowGLFW(GLFWwindow *w,bool is_fullscreen)
+        {
+            glfw_win=w;
+            full_screen=is_fullscreen;
+
+            glfwGetWindowSize(glfw_win,&width,&height);
+        }
+
+        ~RenderWindowGLFW()
+        {
+            glfwDestroyWindow(glfw_win);
+        }
+
+        void ToMin()override{glfwIconifyWindow(glfw_win);}
+        void ToMax()override{glfwMaximizeWindow(glfw_win);}
+
+        void Show()override{glfwShowWindow(glfw_win);}
+        void Hide()override{glfwHideWindow(glfw_win);}
+
+        void SetCaption(const OSString &name) override
+        {
+#if HGL_OS == HGL_OS_Windows
+            glfwSetWindowTitle(glfw_win,to_u8(name));
+#else
+            glfwSetWindowTitle(glfw_win,name);
+#endif//
+            this->caption=name;
+        }
+
+        void SetSize(int w,int h) override
+        {
+            glfwSetWindowSize(glfw_win,w,h);
+            this->width=w;
+            this->height=h;
+        }
+
+        void MakeToCurrent()override{glfwMakeContextCurrent(glfw_win);}
+
+        void SwapBuffer()override{glfwSwapBuffers(glfw_win);}
+
+        void WaitEvent(const double &time_out)override
+        {
+            if(time_out>0)
+                glfwWaitEventsTimeout(time_out);
+            else
+                glfwWaitEvents();
+        }
+
+        void PollEvent()override{glfwPollEvents();}
+
+        bool IsOpen()override{return(!glfwWindowShouldClose(glfw_win));}
+    };//class RenderWindowGLFW:public RenderWindow
+
+    RenderWindow *CreateRenderWindowGLFW(GLFWwindow *win,bool is_fullscreen)
+    {
+        return(new RenderWindowGLFW(win,is_fullscreen));
+    }
+}//namespace hgl
