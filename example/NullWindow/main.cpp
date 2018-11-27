a9db717a768a584d2285879243786f419c471b8c
hyzboy <hyzboy@gmail.com>
2018-11-27 15:43:32 +0800

    增加部分基础库头文件

---
+++ example/NullWindow/main.cpp
@@ -0,0 +1,7 @@
+﻿#include<iostream>
+
+int main(int,char **)
+{
+    std::cout<<"hello,world!"<<std::endl;
+    return 0;
+}
---
+++ inc/hgl/CompOperator.h
@@ -0,0 +1,16 @@
+﻿#ifndef HGL_COMP_OPERATOR_INCLUDE
+#define HGL_COMP_OPERATOR_INCLUDE
+
+namespace hgl
+{
+	#define CompOperator(name,compfunc)	const bool operator > (name i)const 	{return compfunc(i)>0;}	\
+										const bool operator < (name i)const 	{return compfunc(i)<0;}	\
+										const bool operator >=(name i)const 	{return compfunc(i)>=0;}\
+										const bool operator <=(name i)const 	{return compfunc(i)<=0;}\
+										const bool operator ==(name i)const 	{return compfunc(i)==0;}\
+										const bool operator !=(name i)const 	{return compfunc(i)!=0;}
+
+	#define CompOperatorMemcmp(name)	int _Comp(name data)const{return memcmp(this,&data,sizeof(name));}	\
+										CompOperator(name,_Comp)
+}//namespace hgl
+#endif//HGL_COMP_OPERATOR_INCLUDE
