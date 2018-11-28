f3065b26af900e77898a878c4c55b95cfbd1bc31
hyzboy <hyzboy@gmail.com>
2018-11-28 15:19:20 +0800

    增加RenderStatus/Driver等设计

---
+++ src/RenderDriver/GLCore/RenderDriverGLCore.cpp
@@ -0,0 +1,22 @@
+﻿#include<hgl/render/RenderDriver.h>
+
+namespace hgl
+{
+    class RenderDriverGLCore:public RenderDriver
+    {
+    public:
+
+        void SetCurStatus(const RenderStatus &rs) override
+        {
+            if(rs.color
+        }
+
+        void ClearColorBuffer() override
+        {
+        }
+
+        void ClearDepthBuffer() override
+        {
+        }
+    };//class RenderDriverGLCore:public RenderDriver
+}//namespace hgl
