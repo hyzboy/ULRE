6394ad8b7427fee2ad10cbff26841b1a52c54e63
hyzboy <hyzboy@gmail.com>
2018-11-30 19:26:08 +0800

    VertexBuffer相关代码修正至可编译(未做使用测试)

---
+++ src/RenderDriver/VertexBufferControlDSA.cpp
@@ -0,0 +1,38 @@
+﻿#include"VertexBufferControl.h"
+#include<GLEWCore/glew.h>
+
+namespace hgl
+{
+    namespace graph
+    {
+        class VertexBufferControlDSA:public VertexBufferControl
+        {
+        public:
+
+            using VertexBufferControl::VertexBufferControl;
+
+            void Set(int size, void *data,uint data_level)
+            {
+                glNamedBufferData(this->index, size, data, data_level);
+            }
+
+            void Change(int start, int size, void *data)
+            {
+                glNamedBufferSubData(this->index, start, size, data);
+            }
+        };//class VertexBufferControlDSA
+
+        VertexBufferControl *CreateVertexBufferControlDSA(uint type)
+        {
+            uint index;
+
+            glCreateBuffers(1,&index);
+            return(new VertexBufferControlDSA(type,index));
+        }
+
+        void DeleteVertexBufferControlDSA(VertexBufferControl *vbc)
+        {
+            SAFE_CLEAR(vbc);
+        }
+    }//namespace graph
+}//namespace hgl
