d1322a9f9793edeb1f642743f980effcac5c4d64
hyzboy <hyzboy@gmail.com>
2018-11-30 20:04:00 +0800

    增加Axis,BlendMode,Light,Material等定义

---
+++ inc/hgl/graph/Renderable.h
@@ -0,0 +1,21 @@
+﻿#ifndef HGL_GRAPH_RENDERABLE_INCLUDE
+#define HGL_GRAPH_RENDERABLE_INCLUDE
+
+#include<hgl/graph/Shader.h>
+namespace hgl
+{
+    namespace graph
+    {
+        /**
+         * 可渲染对象
+         */
+        class Renderable
+        {
+        protected:
+
+            VertexArray *va;
+            Material *mtl;
+        };//class Renderable
+    }//namespace graph
+}//namespace hgl
+#endif//HGL_GRAPH_RENDERABLE_INCLUDE
