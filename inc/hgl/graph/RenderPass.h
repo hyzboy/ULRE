af2d22b4d542c1a62509c489749d0c7e120457a0
hyzboy <hyzboy@gmail.com>
2018-12-07 16:15:06 +0800

    缺失的头文件

---
+++ inc/hgl/graph/RenderPass.h
@@ -0,0 +1,20 @@
+﻿#ifndef HGL_GRAPH_RENDER_PASS_INCLUDE
+#define HGL_GRAPH_RENDER_PASS_INCLUDE
+
+namespace hgl
+{
+    namespace graph
+    {
+        /**
+         * 单次渲染处理
+         */
+        class RenderPass
+        {
+        public:
+
+
+        };//class RenderPass
+    }//namespace graph
+}//namespace hgl
+#endif//HGL_GRAPH_RENDER_PASS_INCLUDE
+
