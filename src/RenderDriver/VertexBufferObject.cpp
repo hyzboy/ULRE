0ae3c2208d9eb7b36f83fc77d10194cf1c1f4149
hyzboy <hyzboy@gmail.com>
2019-03-18 14:28:34 +0800

    VAO中vertex_改名为position_，SetVertexBuffer改名为SetPositionBuffer，以避开名词理解错误

---
+++ src/RenderDriver/VertexBufferObject.cpp
@@ -0,0 +1,42 @@
+﻿#include<hgl/graph/VertexBufferObject.h>
+
+namespace hgl
+{
+    namespace graph
+    {
+        class VertexBufferObjectDSA:public VertexBufferObject
+        {
+        public:
+
+            using VertexBufferObject::VertexBufferObject;
+            ~VertexBufferObjectDSA()
+            {
+                glDeleteBuffers(1,&buffer_index);
+            }
+
+            void Set(GLsizei size,void *data)
+            {
+                glNamedBufferData(buffer_index,size,data,user_pattern);
+            }
+
+            void Change(GLintptr start,GLsizei size,void *data)
+            {
+                glNamedBufferSubData(buffer_index,start,size,data);
+            }
+        };//class VertexBufferControlDSA
+
+        VertexBufferObject *CreateVertexBufferObjectDSA(uint type)
+        {
+            uint index;
+
+            glCreateBuffers(1,&index);
+        }
+
+        VertexBufferObject *CreateVBO(const GLenum &buf_type,
+                                      const uint &data_type,const uint &data_bytes,const uint &data_comp,
+                                      const GLsizeiptr &size,
+                                      const GLenum &dsup)
+        {
+        }
+    }//namespace graph
+}//namespace hgl
