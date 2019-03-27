5860a911ecc6ae9018d1fc9192c8261b70e2cadf
HuYingzhuo <hyzboy@gmail.com>
2019-03-27 14:08:16 +0800

    删除旧的VertexBuffer

---
+++ discarded/VertexBufferControlDSA.cpp
@@ -0,0 +1,38 @@
+﻿#include"VertexBufferControl.h"
+#include<GLEWCore/glew.h>
+
+namespace hgl
+{
+    namespace graph
+    {
+        class VertexBufferControlDSA:public VertexBufferControl
+        {
+        public:
+
+            using VertexBufferControl::VertexBufferControl;
+
+            void Set(GLsizei size, void *data,GLenum data_usage)
+            {
+                glNamedBufferData(this->index, size, data, data_usage);
+            }
+
+            void Change(GLintptr start, GLsizei size, void *data)
+            {
+                glNamedBufferSubData(this->index, start, size, data);
+            }
+        };//class VertexBufferControlDSA
+
+        VertexBufferControl *CreateVertexBufferControlDSA(uint type)
+        {
+            uint index;
+
+            glCreateBuffers(1,&index);
+            return(new VertexBufferControlDSA(type,index));
+        }
+
+        void DeleteVertexBufferControlDSA(VertexBufferControl *vbc)
+        {
+            SAFE_CLEAR(vbc);
+        }
+    }//namespace graph
+}//namespace hgl
