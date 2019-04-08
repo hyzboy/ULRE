511365d13440e596ac65df417959d7efd0ee79f6
hyzboy <hyzboy@outlook.com>
2019-04-09 02:02:43 +0800

    初步封装Vulkan的Instance,PhysicalDevice,Device,CommandBuffer

---
+++ example/Vulkan/VKCommandBuffer.h
@@ -0,0 +1,19 @@
+﻿#ifndef HGL_GRAPH_VULKAN_COMMAND_BUFFER_INCLUDE
+#define HGL_GRAPH_VULKAN_COMMAND_BUFFER_INCLUDE
+
+#include"VK.h"
+
+VK_NAMESPACE_BEGIN
+    class CommandBuffer
+    {
+        VkDevice device;
+        VkCommandPool pool;
+        VkCommandBuffer buf;
+
+    public:
+
+        CommandBuffer(VkDevice dev,VkCommandPool cp,VkCommandBuffer cb){device=dev;pool=cp;buf=cb;}
+        ~CommandBuffer();
+    };//class CommandBuffer
+VK_NAMESPACE_END
+#endif//HGL_GRAPH_VULKAN_COMMAND_BUFFER_INCLUDE
