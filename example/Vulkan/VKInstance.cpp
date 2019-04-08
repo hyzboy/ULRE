583c4a7df47c4ef69d39b944b8192573710e779b
hyzboy <hyzboy@outlook.com>
2019-04-09 00:22:26 +0800

    开始Vulkan尝试

---
+++ example/Vulkan/VKInstance.cpp
@@ -0,0 +1,52 @@
+﻿#include"VKInstance.h"
+#include<hgl/type/DataType.h>
+
+VK_NAMESPACE_BEGIN
+
+Instance::Instance(const UTF8String &an)
+{
+    app_name=an;
+
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pNext = nullptr;
+    app_info.pApplicationName = app_name.c_str();
+    app_info.applicationVersion = 1;
+    app_info.pEngineName = "CMGameEngine/ULRE";
+    app_info.engineVersion = 1;
+    app_info.apiVersion = VK_API_VERSION_1_0;
+
+    inst_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    inst_info.pNext = nullptr;
+    inst_info.flags = 0;
+    inst_info.pApplicationInfo = &app_info;
+    inst_info.enabledExtensionCount = 0;
+    inst_info.ppEnabledExtensionNames = nullptr;
+    inst_info.enabledLayerCount = 0;
+    inst_info.ppEnabledLayerNames = nullptr;
+
+    inst=nullptr;
+}
+
+Instance::~Instance()
+{
+    if(inst)
+        vkDestroyInstance(inst,nullptr);
+}
+
+bool Instance::Init()
+{
+    if(inst)
+        return(false);
+
+    VkResult res=vkCreateInstance(&inst_info,nullptr,&inst);
+
+    if(res)
+    {
+        inst=nullptr;
+        return(false);
+    }
+
+    return(true);
+}
+
+VK_NAMESPACE_END
---
+++ example/Vulkan/VKInstance.h
@@ -0,0 +1,30 @@
+﻿#ifndef HGL_GRAPH_VULKAN_INSTANCE_INCLUDE
+#define HGL_GRAPH_VULKAN_INSTANCE_INCLUDE
+
+#include<hgl/type/BaseString.h>
+#include<vulkan/vulkan.h>
+
+#define VK_NAMESPACE_BEGIN  namespace hgl{namespace graph{namespace vulkan{
+#define VK_NAMESPACE_END    }}}
+
+VK_NAMESPACE_BEGIN
+    class Instance
+    {
+        VkApplicationInfo app_info;
+        VkInstanceCreateInfo inst_info;
+
+        VkInstance inst;
+
+    private:
+
+        UTF8String app_name;
+
+    public:
+
+        Instance(const UTF8String &an);
+        virtual ~Instance();
+
+        virtual bool Init();
+    };//class Instance
+VK_NAMESPACE_END
+#endif//HGL_GRAPH_VULKAN_INSTANCE_INCLUDE
