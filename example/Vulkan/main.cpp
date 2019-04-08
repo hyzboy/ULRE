37c0501bed1f09b75a91f007ef4c91d8aee3f029
hyzboy <hyzboy@outlook.com>
2019-04-09 00:22:26 +0800

    开始Vulkan尝试

---
+++ example/Vulkan/main.cpp
@@ -0,0 +1,13 @@
+﻿#include"VKInstance.h"
+
+int main(int,char **)
+{
+    using namespace hgl;
+    using namespace hgl::graph;
+
+    vulkan::Instance inst("Test");
+
+    inst.Init();
+
+    return 0;
+}
