e531edfae109787ad014c85ab3ca214a91175d02
hyzboy <hyzboy@gmail.com>
2019-04-16 02:23:03 +0800

    增加新的VertexInput类，用于存储vbo输入数据布局

--- example/Vulkan/VKCommandBuffer.h
+++ example/Vulkan/VKCommandBuffer.h
@@ -2,6 +2,7 @@
 #define HGL_GRAPH_VULKAN_COMMAND_BUFFER_INCLUDE
 
 #include"VK.h"
+#include"VKVertexInput.h"
 
 VK_NAMESPACE_BEGIN
     class CommandBuffer
@@ -14,6 +15,15 @@ VK_NAMESPACE_BEGIN
 
         CommandBuffer(VkDevice dev,VkCommandPool cp,VkCommandBuffer cb){device=dev;pool=cp;buf=cb;}
         ~CommandBuffer();
+
+        void Bind(VertexInput *vi)
+        {
+            auto &buf_list=vi->GetBufferList();
+
+            constexpr VkDeviceSize offsets[1]={0};
+
+            vkCmdBindVertexBuffers(buf,0,buf_list.GetCount(),buf_list.GetData(),offsets);
+        }
     };//class CommandBuffer
 VK_NAMESPACE_END
 #endif//HGL_GRAPH_VULKAN_COMMAND_BUFFER_INCLUDE
---
+++ example/Vulkan/VKVertexInput.cpp
@@ -0,0 +1,38 @@
+﻿#include"VKVertexInput.h"
+
+VK_NAMESPACE_BEGIN
+
+//struct VertexInputBuffer
+//{
+//    //按API，可以一个binding绑多个attrib，但我们仅支持1v1
+//
+//    VkVertexInputBindingDescription binding;
+//    VkVertexInputAttributeDescription attrib;
+//    Buffer *buf;
+//};
+
+bool VertexInput::Add(VertexBuffer *buf)
+{
+    if(!buf)
+        return(false);
+
+    const int binding_index=vib_list.GetCount();      //参考opengl vab,binding_index必须从0开始，紧密排列，但是否必须这样，待以后测试
+
+    VkVertexInputBindingDescription binding;
+    VkVertexInputAttributeDescription attrib;
+
+    binding.binding=binding_index;
+    binding.stride=buf->GetStride();
+    binding.inputRate=VK_VERTEX_INPUT_RATE_VERTEX;      //还有一种是INSTANCE，暂时未知
+
+    attrib.binding=binding_index;
+    attrib.location=0;
+    attrib.format=buf->GetFormat();
+    attrib.offset=0;
+
+    vib_list.Add(new VertexInputBuffer(binding,attrib,buf));
+    buf_list.Add(buf->GetBuffer());
+
+    return(true);
+}
+VK_NAMESPACE_END
