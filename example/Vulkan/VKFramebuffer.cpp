08e353b07a584c27678fb9139baa7a735594c30f
HuYingzhuo <hyzboy@gmail.com>
2019-04-18 21:42:22 +0800

    增加VKFramebuffer

---
+++ example/Vulkan/VKFramebuffer.cpp
@@ -0,0 +1,35 @@
+#include"VKFramebuffer.h"
+VK_NAMESPACE_BEGIN
+Framebuffer::~Framebuffer()
+{
+    vkDestroyFramebuffer(device,frame_buffer,nullptr);
+}
+
+Framebuffer *CreateFramebuffer(Device *dev,RenderPass *rp,VkImageView color,VkImageView depth)
+{
+    if(!dev||!rp||!color||!depth)return(nullptr);
+
+    const VkExtent2D extent=dev->GetExtent();
+    VkImageView attachments[2];
+
+    attachments[0]=color;
+    attachments[1]=depth;
+
+    VkFramebufferCreateInfo fb_info = {};
+    fb_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+    fb_info.pNext = nullptr;
+    fb_info.renderPass = *rp;
+    fb_info.attachmentCount = 2;
+    fb_info.pAttachments = attachments;
+    fb_info.width = extent.width;
+    fb_info.height = extent.height;
+    fb_info.layers = 1;
+
+    VkFramebuffer fb;
+
+    if(vkCreateFramebuffer(dev->GetDevice(), &fb_info, nullptr, &fb)!=VK_SUCCESS)
+        return(nullptr);
+
+    return(new Framebuffer(dev->GetDevice(),fb));
+}
+VK_NAMESPACE_END
