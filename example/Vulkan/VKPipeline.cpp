e588213948d4cdef48a810501e3446c6d00b0053
hyzboy <hyzboy@gmail.com>
2019-04-21 02:36:47 +0800

    改为默认开启深度测试

--- example/Vulkan/VKPipeline.cpp
+++ example/Vulkan/VKPipeline.cpp
@@ -56,8 +56,8 @@ PipelineCreater::PipelineCreater(Device *dev)
     depthStencilState.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
     depthStencilState.pNext = nullptr;
     depthStencilState.flags = 0;
-    depthStencilState.depthTestEnable = VK_FALSE;
-    depthStencilState.depthWriteEnable = VK_FALSE;
+    depthStencilState.depthTestEnable = VK_TRUE;
+    depthStencilState.depthWriteEnable = VK_TRUE;
     depthStencilState.depthCompareOp = VK_COMPARE_OP_LESS_OR_EQUAL;
     depthStencilState.depthBoundsTestEnable = VK_FALSE;
     depthStencilState.minDepthBounds = 0;
