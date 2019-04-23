5e9bb09621c65173f238fda9126554af8e254377
hyzboy <hyzboy@gmail.com>
2019-04-24 00:35:56 +0800

    拆分数学功能定义

---
+++ inc/hgl/math/FastTriangle.h
@@ -0,0 +1,23 @@
+﻿#ifndef HGL_ALGORITHM_MATH_FAST_TRIANGLE_FUNCTION_INCLUDE
+#define HGL_ALGORITHM_MATH_FAST_TRIANGLE_FUNCTION_INCLUDE
+
+namespace hgl
+{
+    double Lsin(int angle);                                     ///<低精度sin计算,注意传入的参数为角度而非弧度
+    double Lcos(int angle);                                     ///<低精度cos计算,注意传入的参数为角度而非弧度
+    void Lsincos(int angle, double &s, double &c);              ///<低精度sin+cos计算,注意传入的参数为角度而非弧度
+
+    /**
+     * 低精度atan函数
+     */
+    double inline Latan(double z)
+    {
+        constexpr double n1 = 0.97239411f;
+        constexpr double n2 = -0.19194795f;
+
+        return (n1 + n2 * z * z) * z;
+    }
+
+    double Latan2(double y, double x);                          ///<低精度atan2函数
+}//namespace hgl
+#endif//HGL_ALGORITHM_MATH_FAST_TRIANGLE_FUNCTION_INCLUDE
---
+++ inc/hgl/math/Matrix.h
@@ -0,0 +1,153 @@
+﻿#ifndef HGL_ALGORITHM_MATH_VECTOR_MATRIX_INCLUDE
+#define HGL_ALGORITHM_MATH_VECTOR_MATRIX_INCLUDE
+
+#include<hgl/math/Vector.h>
+
+//注：GLM/CML(OpenGLMode)是列矩阵,计算坐标matrix*pos
+//   而MGL是行矩阵，需要反过来pos*matrix
+
+namespace hgl
+{
+    using Matrix3f=float3x3;
+    using Matrix4f=float4x4;
+
+    inline Matrix4f identity()
+    {
+        return Matrix4f::identity;
+    }
+
+    inline Matrix4f inverse(const Matrix4f &m)
+    {
+        return m.Inverted();
+    }
+
+    inline Matrix4f ortho(  float left,
+                            float right,
+                            float bottom,
+                            float top,
+                            float znear,
+                            float zfar )
+    {
+        return Matrix4f(
+            2.0f / (right - left),  0.0f,                   0.0f,                   -(right + left) / (right - left),
+            0.0f,                   2.0f / (bottom - top),  0.0f,                   -(bottom + top) / (bottom - top),
+            0.0f,                   0.0f,                   1.0f / (znear - zfar),  znear / (znear - zfar),
+            0.0f,                   0.0f,                   0.0f,                   1.0f);
+    }
+
+    /**
+     * 生成一个正角视图矩阵
+     * @param width 宽
+     * @param height 高
+     * @param znear 近平面z值
+     * @param zfar 远平台z值
+     */
+    inline Matrix4f ortho(float width,float height,float znear,float zfar)
+    {
+        return Matrix4f(
+            2.0f / width,   0.0f,           0.0f,                   -1,
+            0.0f,           2.0f / height,  0.0f,                   -1,
+            0.0f,           0.0f,           1.0f / (znear - zfar),  znear / (znear - zfar),
+            0.0f,           0.0f,           0.0f,                   1.0f);
+    }
+
+    /**
+     * 生成一个正角视图矩阵
+     * @param width 宽
+     * @param height 高
+     */
+    inline Matrix4f ortho(float width,float height)
+    {
+        return Matrix4f(
+            2.0f / width,   0.0f,           0.0f,   -1,
+            0.0f,           2.0f / height,  0.0f,   -1,
+            0.0f,           0.0f,           -1.0f , 0.0f,
+            0.0f,           0.0f,           0.0f,   1.0f);
+    }
+
+    /**
+     * 生成一个透视矩阵
+     * @param aspect_ratio 宽高比
+     * @param field_of_view 视野
+     * @param znear 近截面
+     * @param zfar 远截面
+     */
+    inline Matrix4f perspective(float aspect_ratio,
+                                float field_of_view,
+                                float znear,
+                                float zfar)
+    {
+        const float f = 1.0f / tan( hgl_ang2rad( 0.5f * field_of_view ) );
+
+        return Matrix4f(
+            f / aspect_ratio,   0.0f,   0.0f,                   0.0f,
+            0.0f,               -f,     0.0f,                   0.0f,
+            0.0f,               0.0f,   zfar / (znear - zfar),  (znear * zfar) / (znear - zfar),
+            0.0f,               0.0f,   -1.0f,                  0.0f);
+    }
+
+    /**
+     * 生成一个透视矩阵
+     * @param aspect_ratio 宽高比
+     * @param field_of_view 视野
+     */
+    inline Matrix4f perspective(float aspect_ratio,
+                                float field_of_view=45.0f)
+    {
+        const float f = 1.0f / tan( hgl_ang2rad( 0.5f * field_of_view ) );
+
+        return Matrix4f(
+            f / aspect_ratio,   0.0f,   0.0f,   0.0f,
+            0.0f,               -f,     0.0f,   0.0f,
+            0.0f,               0.0f,   -1.0f,  0.0f,
+            0.0f,               0.0f,   -1.0f,  0.0f);
+    }
+
+    inline Matrix4f translate(const Vector3f &v)
+    {
+        return Matrix4f::Translate(v);
+    }
+
+    inline Matrix4f translate(float x,float y,float z)
+    {
+        return Matrix4f::Translate(x,y,z);
+    }
+
+    inline Matrix4f scale(const Vector3f &v)
+    {
+        return Matrix4f::Scale(v,Vector3f::zero);
+    }
+
+    inline Matrix4f scale(float x,float y,float z)
+    {
+        return Matrix4f::Scale(Vector3f(x,y,z),Vector3f::zero);
+    }
+
+    inline Matrix4f scale(float s)
+    {
+        return Matrix4f::Scale(Vector3f(s,s,s),Vector3f::zero);
+    }
+
+    inline Matrix4f rotate(float angle,const Vector3f &axis)
+    {
+        return Matrix4f::RotateAxisAngle(axis.Normalized(),angle);
+    }
+
+    inline Matrix4f rotate(float angle,float x,float y,float z)
+    {
+        return rotate(angle,Vector3f(x,y,z));
+    }
+
+    inline Matrix4f rotate(float angle,const Vector4f &axis)
+    {
+        return rotate(angle,Vector3f(axis.x,axis.y,axis.z));
+    }
+
+    inline Vector3f rotate(const Vector3f &v3f,float angle,const Vector3f &axis)
+    {
+        Vector4f result=rotate(angle,axis)*Vector4f(v3f,1.0f);
+
+        return result.xyz();
+    }
+}//namespace hgl
+#endif//HGL_ALGORITHM_MATH_VECTOR_MATRIX_INCLUDE
