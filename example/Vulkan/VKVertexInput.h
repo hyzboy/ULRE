d485dd90e3f6d920d73576aa950afdf8958e4ca3
hyzboy <hyzboy@gmail.com>
2019-04-26 03:03:21 +0800

    通过SPRIV-Cross获取shader中顶点输入变量名称和类型等，自动创建VertexInputState。提供根据名称获取binding,location的功能

--- example/Vulkan/VKVertexInput.h
+++ example/Vulkan/VKVertexInput.h
@@ -2,6 +2,8 @@
 #define HGL_GRAPH_VULKAN_VERTEX_INPUT_INCLUDE
 
 #include"VK.h"
+#include<hgl/type/BaseString.h>
+#include<hgl/type/Map.h>
 VK_NAMESPACE_BEGIN
 class VertexBuffer;
 class IndexBuffer;
@@ -15,16 +17,47 @@ class VertexInputState
     List<VkVertexInputBindingDescription> binding_list;
     List<VkVertexInputAttributeDescription> attribute_list;
 
+    struct StageInput
+    {
+        int binding;
+        VkFormat format;
+
+        uint32_t location;
+
+    public:
+
+        StageInput(){}
+        StageInput(const int b,const uint32_t l,const VkFormat f)
+        {
+            binding=b;
+            location=l;
+            format=f;
+        }
+
+        CompOperatorMemcmp(const StageInput &);
+    };//struct StageInput
+
+    Map<UTF8String,StageInput> stage_input_locations;
 public:
 
     VertexInputState()=default;
     ~VertexInputState()=default;
 
-    void Add(const uint32_t shader_location,const VkFormat format,uint32_t offset=0,bool instance=false);
+    int Add(const UTF8String &name,const uint32_t shader_location,const VkFormat format,uint32_t offset=0,bool instance=false);
 
 public:
 
+    void Clear()
+    {
+        binding_list.Clear();
+        attribute_list.Clear();
+    }
+
     const uint32_t GetCount()const{return binding_list.GetCount();}
+
+
+    const int                           GetLocation (const UTF8String &)const;
+    const int                           GetBinding  (const UTF8String &)const;
 
     VkVertexInputBindingDescription *   GetDesc(const int index){return (index<0||index>=binding_list.GetCount()?nullptr:binding_list.GetData()+index);}
     VkVertexInputAttributeDescription * GetAttr(const int index){return (index<0||index>=attribute_list.GetCount()?nullptr:attribute_list.GetData()+index);}
@@ -40,7 +73,7 @@ class VertexInput
 {
     VertexInputState *vis;
 
-    uint32_t buf_count=0;
+    int buf_count=0;
     VkBuffer *buf_list=nullptr;
     VkDeviceSize *buf_offset=nullptr;
     
@@ -52,7 +85,9 @@ public:
     VertexInput(VertexInputState *);
     virtual ~VertexInput();
 
-    bool Set(uint32_t index,VertexBuffer *,VkDeviceSize offset=0);
+    bool Set(int binding,VertexBuffer *,VkDeviceSize offset=0);
+    bool Set(const UTF8String &name,VertexBuffer *vb,VkDeviceSize offset=0){return Set(vis->GetBinding(name),vb,offset);}
+
     bool Set(IndexBuffer *ib,VkDeviceSize offset=0)
     {
         if(!ib)return(false);
@@ -64,7 +99,7 @@ public:
 
 public:
 
-    const uint32_t          GetCount    ()const{return buf_count;}
+    const int               GetCount    ()const{return buf_count;}
     const VkBuffer *        GetBuffer   ()const{return buf_list;}
     const VkDeviceSize *    GetOffset   ()const{return buf_offset;}
 
