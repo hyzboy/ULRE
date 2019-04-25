ffcf03dfd35d37aaa948fe5e1f7ab57888c1be78
HuYingzhuo <hyzboy@gmail.com>
2019-04-25 10:09:56 +0800

    定义Material/MaterialInstance

---
+++ example/Vulkan/VKMaterial.cpp
@@ -0,0 +1,12 @@
+﻿#include"VKMaterial.h"
+VK_NAMESPACE_BEGIN
+Material::~Material()
+{
+    delete dsl_creater;
+    delete shader;
+}
+
+MaterialInstance *Material::CreateInstance()
+{
+}
+VK_NAMESPACE_END
