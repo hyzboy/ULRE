176d9661595c3f4fdba1247a6814cbcf67a4ad0b
hyzboy <hyzboy@gmail.com>
2019-04-27 01:36:58 +0800

    Shader增加UBO binding记录和获取

--- example/Vulkan/VKMaterial.cpp
+++ example/Vulkan/VKMaterial.cpp
@@ -5,19 +5,23 @@
 VK_NAMESPACE_BEGIN
 Material::~Material()
 {
-    delete dsl_creater;
     delete shader;
 }
 
 MaterialInstance *Material::CreateInstance()
 {
-    VertexAttributeBinding *vab=shader->CreateVertexAttributeBinding();
+    return(new MaterialInstance(this));
+}
 
-    return(new MaterialInstance(this,vab));
+MaterialInstance::MaterialInstance(Material *m)
+{
+    mat=m;
+    vab=->CreateVertexAttributeBinding();
 }
 
 MaterialInstance::~MaterialInstance()
 {
     delete vab;
+    delete dsl_creater;
 }
 VK_NAMESPACE_END
