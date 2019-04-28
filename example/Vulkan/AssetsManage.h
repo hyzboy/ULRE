214f1b8abce0591c0da18ae9019205f80f04de65
HuYingzhuo <hyzboy@gmail.com>
2019-04-28 16:03:27 +0800

    添加资产管理，暂时只有加载文件到内存

---
+++ example/Vulkan/AssetsManage.h
@@ -0,0 +1,6 @@
+#ifndef HGL_ASSETS_MANAGE_INCLUDE
+#define HGL_ASSETS_MANAGE_INCLUDE
+
+char *LoadFileToMemory(const char *filename,unsigned __int32 &file_length);
+
+#endif//HGL_ASSETS_MANAGE_INCLUDE
