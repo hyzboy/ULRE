8248b4aef4ae773aa12d5325bccf43adaeeee6e3
hyzboy <hyzboy@gmail.com>
2019-05-05 01:28:01 +0800

    增加下一步工作计划说明

---
+++ WIP.cpp
@@ -0,0 +1,16 @@
+﻿2019.05.05 00:27
+
+    需建立Mesh类，独立提供数据，但又与Renderable类不同。
+    Mesh类提供原始的缓冲区数据与图元类型
+
+    Renderable提供可供Vulkan使用的缓冲区类型，并且其中可能包含多个MESH的数据
+    比如有多种形状的石头，它们使用了同一种材质，这种情况下多个mesh就可以合并到一个Renderable中，渲染时不再切换VBO。
+
+    SceneNode提供当前节点的变换矩阵，以及mesh、灯光、摄像机等信息。
+
+    工作计划：
+
+        1.使用yaml保存pipeline信息
+        2.建立scene数据文件。
+            scene主体依然使用yaml描述
+            mesh,信息使用二进制独立保存
