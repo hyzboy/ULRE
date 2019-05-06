40b57bf7a7415d98e05e65833dbeb2e7cd77bb32
hyzboy <hyzboy@gmail.com>
2019-05-07 03:18:39 +0800

    添加Linux系统下缺少的文件

---
+++ src/Platform/UNIX/XCBVulkan.cpp
@@ -0,0 +1,26 @@
+﻿#include"XCBWindow.h"
+#include<hgl/graph/vulkan/VK.h>
+#include<vulkan/vulkan_xcb.h>
+
+VK_NAMESPACE_BEGIN
+VkSurfaceKHR CreateRenderDevice(VkInstance vk_inst,Window *win)
+{
+    XCBWindow *xcb_win=(XCBWindow *)win;
+
+    VkXcbSurfaceCreateInfoKHR createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR;
+    createInfo.pNext = nullptr;
+    createInfo.connection = xcb_win->GetConnection();
+    createInfo.window = xcb_win->GetWindow();
+
+    VkSurfaceKHR surface;
+
+    VkResult res = vkCreateXcbSurfaceKHR(vk_inst, &createInfo, nullptr, &surface);
+
+    if (res != VK_SUCCESS)
+        return(nullptr);
+
+    return(surface);
+}
+VK_NAMESPACE_END
+
