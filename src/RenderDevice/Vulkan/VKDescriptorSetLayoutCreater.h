fe1eb600fb7a142ba4b97b5dbdd9fdcdf0f76fb6
hyzboy <hyzboy@gmail.com>
2019-05-06 22:33:21 +0800

    改进Shader中的资源管理，改为一次注册所有数据

--- src/RenderDevice/Vulkan/VKDescriptorSetLayoutCreater.h
+++ src/RenderDevice/Vulkan/VKDescriptorSetLayoutCreater.h
@@ -1,7 +1,6 @@
 ﻿#pragma once
 
-#include<hgl/graph/vulkan/VK.h>
-#include<hgl/type/Map.h>
+#include<hgl/graph/vulkan/VKShaderResource.h>
 VK_NAMESPACE_BEGIN
 class Device;
 class DescriptorSets;
@@ -26,27 +25,33 @@ public:
 
     void Bind(const uint32_t binding,VkDescriptorType,VkShaderStageFlagBits);
     void Bind(const uint32_t *binding,const uint32_t count,VkDescriptorType type,VkShaderStageFlagBits stage);
+    void Bind(const ShaderResourceList &srl,VkDescriptorType type,VkShaderStageFlagBits stage){Bind(srl.binding_list.GetData(),srl.binding_list.GetCount(),type,stage);}
+    void Bind(const ShaderResource &sr,VkShaderStageFlagBits stage)
+    {
+        for(uint32_t i=VK_DESCRIPTOR_TYPE_BEGIN_RANGE;i<=VK_DESCRIPTOR_TYPE_END_RANGE;i++)
+            Bind(sr[i],(VkDescriptorType)i,stage);
+    }
 
-#define DESC_SET_BIND_FUNC(name,vkname) void Bind##name(const uint32_t binding,VkShaderStageFlagBits stage_flag){Bind(binding,VK_DESCRIPTOR_TYPE_##vkname,stage_flag);} \
-                                        void Bind##name(const uint32_t *binding,const uint32_t count,VkShaderStageFlagBits stage_flag){Bind(binding,count,VK_DESCRIPTOR_TYPE_##vkname,stage_flag);}
-
-    DESC_SET_BIND_FUNC(Sampler,     SAMPLER);
-    DESC_SET_BIND_FUNC(UBO,         UNIFORM_BUFFER);
-    DESC_SET_BIND_FUNC(SSBO,        STORAGE_BUFFER);
-
-    DESC_SET_BIND_FUNC(CombinedImageSampler,    COMBINED_IMAGE_SAMPLER);
-    DESC_SET_BIND_FUNC(SampledImage,            SAMPLED_IMAGE);
-    DESC_SET_BIND_FUNC(StorageImage,            STORAGE_IMAGE);
-    DESC_SET_BIND_FUNC(UniformTexelBuffer,      UNIFORM_TEXEL_BUFFER);
-    DESC_SET_BIND_FUNC(StorageTexelBuffer,      STORAGE_TEXEL_BUFFER);
-
-
-    DESC_SET_BIND_FUNC(UBODynamic,  UNIFORM_BUFFER_DYNAMIC);
-    DESC_SET_BIND_FUNC(SBODynamic,  STORAGE_BUFFER_DYNAMIC);
-
-    DESC_SET_BIND_FUNC(InputAttachment,  INPUT_ATTACHMENT);
-
-#undef DESC_SET_BIND_FUNC
+//
+//#define DESC_SET_BIND_FUNC(name,vkname) void Bind##name(const uint32_t binding,VkShaderStageFlagBits stage_flag){Bind(binding,VK_DESCRIPTOR_TYPE_##vkname,stage_flag);} \
+//                                        void Bind##name(const uint32_t *binding,const uint32_t count,VkShaderStageFlagBits stage_flag){Bind(binding,count,VK_DESCRIPTOR_TYPE_##vkname,stage_flag);}
+//
+//    DESC_SET_BIND_FUNC(Sampler,                 SAMPLER);
+//
+//    DESC_SET_BIND_FUNC(CombinedImageSampler,    COMBINED_IMAGE_SAMPLER);
+//    DESC_SET_BIND_FUNC(SampledImage,            SAMPLED_IMAGE);
+//    DESC_SET_BIND_FUNC(StorageImage,            STORAGE_IMAGE);
+//
+//    DESC_SET_BIND_FUNC(UTBO,                    UNIFORM_TEXEL_BUFFER);
+//    DESC_SET_BIND_FUNC(SSTBO,                   STORAGE_TEXEL_BUFFER);
+//    DESC_SET_BIND_FUNC(UBO,                     UNIFORM_BUFFER);
+//    DESC_SET_BIND_FUNC(SSBO,                    STORAGE_BUFFER);
+//    DESC_SET_BIND_FUNC(UBODynamic,              UNIFORM_BUFFER_DYNAMIC);
+//    DESC_SET_BIND_FUNC(SSBODynamic,             STORAGE_BUFFER_DYNAMIC);
+//
+//    DESC_SET_BIND_FUNC(InputAttachment,         INPUT_ATTACHMENT);
+//
+//#undef DESC_SET_BIND_FUNC
 
     bool CreatePipelineLayout();
 
