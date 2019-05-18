0879f7fad31e34cef9476b12307c4149ecdfda17
hyzboy <hyzboy@gmail.com>
2019-05-18 16:10:42 +0800

    增加Device::CreateSampler

--- inc/hgl/graph/vulkan/VKDevice.h
+++ inc/hgl/graph/vulkan/VKDevice.h
@@ -89,6 +89,7 @@ public: //Buffer相关
 public: //material相关
 
     Texture2D *CreateTexture2D(const VkFormat video_format,void *data,uint32_t width,uint32_t height,uint32_t size,bool force_linear=false);
+    Sampler *CreateSampler(VkSamplerCreateInfo *);
 
     ShaderModuleManage *CreateShaderModuleManage();
 
