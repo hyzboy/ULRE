3bbe7e56ec2407c9902c31d15e94d2985ac1cb64
hyzboy <hyzboy@gmail.com>
2019-05-28 21:44:02 +0800

    增加AndroidVulkan.cpp

---
+++ src/Platform/Android/AndroidVulkan.cpp
@@ -0,0 +1,29 @@
+﻿#include"AndroidWindow.h"
+#include<vulkan/vulkan_android.h>
+
+namespace hgl
+{
+    VkSurfaceKHR AndroidWindow::CreateSurface(VkInstance vk_inst)
+    {
+        PFN_vkCreateAndroidSurfaceKHR CreateAndroidSurfaceKHR;
+
+        GET_INSTANCE_PROC_ADDR(vk_inst,CreateAndroidSurfaceKHR);
+
+        if(!CreateAndroidSurfaceKHR)
+            return(nullptr);
+
+        VkAndroidSurfaceCreateInfoKHR createInfo;
+        createInfo.sType = VK_STRUCTURE_TYPE_ANDROID_SURFACE_CREATE_INFO_KHR;
+        createInfo.pNext = nullptr;
+        createInfo.window = AndroidGetApplicationWindow();
+
+        VkSurfaceKHR surface;
+
+        VkResult res=CreateAndroidSurfaceKHR(vk_inst,&createInfo,nullptr,&surface);
+
+        if(res!=VK_SUCCESS)
+            return(nullptr);
+
+        return(surface);
+    }
+}//namespace hgl
