417ddbf060e436b301dcc4d0e3526a700aac7bb5
hyzboy <hyzboy@gmail.com>
2019-08-14 16:36:02 +0800

    完整的纹数据结构输出

---
+++ inc/hgl/graph/vulkan/VKNamespace.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include<vulkan/vulkan.h>
+
+#define VK_NAMESPACE        hgl::graph::vulkan
+
+#define VK_NAMESPACE_USING  using namespace VK_NAMESPACE;
+
+#define VK_NAMESPACE_BEGIN  namespace hgl{namespace graph{namespace vulkan{
+#define VK_NAMESPACE_END    }}}
