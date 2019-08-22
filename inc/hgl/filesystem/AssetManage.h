f5d663f9a5d696d49fb5df2d7f47840da1bacf76
hyzboy <hyzboy@gmail.com>
2019-08-22 11:39:21 +0800

    删除ULRE.Base代码以及cmake module，改为使用git submodule模式引用

--- inc/hgl/filesystem/AssetManage.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef HGL_FILESYSTEM_ASSET_MANAGE_INCLUDE
-#define HGL_FILESYSTEM_ASSET_MANAGE_INCLUDE
-
-#include<hgl/type/BaseString.h>
-namespace hgl
-{
-
-}//namespace hgl
-#endif//HGL_FILESYSTEM_ASSET_MANAGE_INCLUDE
--- inc/hgl/math/Math.h
+++
@@ -1,8 +0,0 @@
-﻿#ifndef HGL_ALGORITHM_MATH_INCLUDE
-#define HGL_ALGORITHM_MATH_INCLUDE
-
-#include<hgl/math/FastTriangle.h>
-#include<hgl/math/Vector.h>        // Game Math and Geometry Library
-#include<hgl/math/Matrix.h>
-
-#endif//HGL_ALGORITHM_MATH_INCLUDE
