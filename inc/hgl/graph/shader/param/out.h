463482a9b37aeeed7a40b175894f7c589ba188ad
hyzboy <hyzboy@gmail.com>
2019-12-13 11:20:08 +0800

    first finished check function of ShaderMaker.wait test.

--- inc/hgl/graph/shader/param/out.h
+++ inc/hgl/graph/shader/param/out.h
@@ -2,21 +2,16 @@
 #define HGL_GRAPH_SHADER_PARAM_OUTPUT_INCLUDE
 
 #include<hgl/graph/shader/param/param.h>
-#include<hgl/type/Set.h>
-#include<hgl/type/Pair.h>
 
 BEGIN_SHADER_PARAM_NAMESPACE
 
 using namespace hgl;
-
-#define SHADER_OUTPUT_PARAM(name,type) output_params.Add(new SHADER_PARAM_NAMESPACE::Param(#name,SHADER_PARAM_NAMESPACE::ParamType::type));
 
 /**
  * 输出参数定义
  */
 class OutputParam:public Param
 {
-
 public:
 
     using Param::Param;
--- src/RenderDevice/Shader/ShaderMaker.cpp
+++ src/RenderDevice/Shader/ShaderMaker.cpp
@@ -1,4 +1,6 @@
 ﻿#include<hgl/graph/shader/ShaderMaker.h>
+#include<hgl/graph/shader/param/in.h>
+#include<hgl/type/Set.h>
 
 BEGIN_SHADER_NAMESPACE
 bool ShaderMaker::Make()
@@ -6,8 +8,45 @@ bool ShaderMaker::Make()
     if(!fin_node)
         return(false);
 
-    if(!fin_node->CheckInputParam())
-        return(false);
+    Set<node::Node *> post;             //完成检测的节点合集
+    Set<node::Node *> prev;             //等待检测的节点合集
+    node::Node *cur,*nn;
+
+    node::InputParamList *ipl;
+    int i,count;
+    param::InputParam **ip;
+
+    prev.Add(fin_node);
+
+    while(prev.GetEnd(cur))
+    {
+        ipl     =&(cur->GetInputParamList());
+        count   =ipl->GetCount();
+        ip      =ipl->GetData();
+
+        for(i=0;i<count;i++)
+        {
+            if(!(*ip)->Check())
+                return(false);
+
+            nn=(*ip)->GetJoinNode();
+
+            if(nn)
+            {
+                if(!post.IsMember(nn)
+                 &&!prev.IsMember(nn)
+                 &&nn!=cur)
+                {
+                    prev.Add(nn);
+                }
+            }
+
+            ip++;
+        }
+        
+        prev.Delete(cur);
+        post.Add(cur);
+    }
 
     return(true);
 }
