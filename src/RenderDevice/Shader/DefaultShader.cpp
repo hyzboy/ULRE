8d8201b1cc8422332d0da0832cc7f5403bcc8ddf
hyzboy <hyzboy@gmail.com>
2019-12-14 13:42:16 +0800

    to improved the CreateDefaultVertexShader()

--- src/RenderDevice/Shader/DefaultShader.cpp
+++ src/RenderDevice/Shader/DefaultShader.cpp
@@ -7,20 +7,24 @@
 BEGIN_SHADER_NAMESPACE
 namespace
 {
-    #define SHADER_VERTEX_INPUT_POSITION    "Position"
+    #define SHADER_VERTEX_INPUT_STREAM_PARAM_NAME    "Position"      //<顶点数据输入流，输出参数名称
+    #define SHADER_VERTEX_POSITION_INPUT_PARAM_NAME  "Position"      //<顶点shader最终节点，输入参数名称
+    
+    //**********************************************************************注：它们两个名字一样只是碰巧****************
 
     bool CreateDefaultVertexShader()
     {
-        node::VertexFinished vs_fin_node;                       //创建一个vs最终节点
+        node::VertexFinished vs_fin_node;                                           //创建一个vs最终节点
 
-                                                                //创建一个顶点输入节点
-        node::VertexInput ni(   SHADER_VERTEX_INPUT_POSITION,   //该节点shader名称
-                                param::ParamType::Float3);      //该节点数据类型
+                                                                                    //创建一个顶点输入节点
+        node::VertexInput ni(   SHADER_VERTEX_INPUT_STREAM_PARAM_NAME,              //该节点shader名称
+                                param::ParamType::Float3);                          //该节点数据类型
 
-                                                                //对接顶点输入节点到VS最终节点
-        vs_fin_node.JoinInput(  SHADER_VERTEX_INPUT_POSITION,   //最终节点名称
-                                &ni,                            //要接入的节点
-                                SHADER_VERTEX_INPUT_POSITION);  //要入节点的输出参数名称       (注：这里只是碰巧名字一样，所以共用一个宏)
+        param::OutputParam *op=ni.GetOutput(SHADER_VERTEX_INPUT_STREAM_PARAM_NAME); //获取节点中的输出参数
+
+                                                                                    //对接顶点输入节点到VS最终节点
+        vs_fin_node.JoinInput(  SHADER_VERTEX_POSITION_INPUT_PARAM_NAME,            //最终节点中这个输入参数的名称
+                                &ni,op);                                            //要接入的节点以及它的输出参数
 
         ShaderMaker vs_maker(&vs_fin_node);
 
