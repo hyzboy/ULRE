3398126e237e3d2a628d7832cb470d5286fdb941
hyzboy <hyzboy@gmail.com>
2020-01-09 16:53:30 +0800

    add new MaterialComponent

---
+++ inc/hgl/graph/MaterialComponent.h
@@ -0,0 +1,87 @@
+﻿#ifndef HGL_GRAPH_MATERIAL_COMPONENT_INCLUDE
+#define HGL_GRAPH_MATERIAL_COMPONENT_INCLUDE
+
+#include<hgl/type/DataType.h>
+
+#define BEGIN_MATERIAL_NAMESPACE namespace hgl{namespace graph{namespace material{
+#define END_MATERIAL_NAMESPACE }}}
+
+BEGIN_MATERIAL_NAMESPACE
+    enum class Component
+    {
+        ShadingModel=0,
+
+        Color,
+        Opacity,
+        Normal,
+
+        Metallic,
+        Roughness,
+
+        Emissive,
+        Refraction,
+
+        AO,
+        SSS,
+        Height,
+
+        RIM,
+        ClearCoat,
+        Anisotropy,
+
+        BEGIN_RANGE =ShadingModel,
+        END_RANGE   =Anisotropy,
+        RANGE_SIZE  =END_RANGE-BEGIN_RANGE+1,
+    };//
+
+    enum class ComponentBit
+    {
+    #define MC_BIT_DEFINE(name) name=1<<(uint)Component::name
+        MC_BIT_DEFINE(ShadingModel      ),
+
+        MC_BIT_DEFINE(Color             ),
+        MC_BIT_DEFINE(Opacity           ),
+        MC_BIT_DEFINE(Normal            ),
+
+        MC_BIT_DEFINE(Metallic          ),
+        MC_BIT_DEFINE(Roughness         ),
+
+        MC_BIT_DEFINE(Emissive          ),
+        MC_BIT_DEFINE(Refraction        ),
+
+        MC_BIT_DEFINE(AO                ),
+        MC_BIT_DEFINE(SSS               ),
+        MC_BIT_DEFINE(Height            ),
+
+        MC_BIT_DEFINE(RIM               ),
+        MC_BIT_DEFINE(ClearCoat         ),
+        MC_BIT_DEFINE(Anisotropy        ),
+    #undef MC_BIT_DEFINE
+    };//enum class ComponentBit
+
+    enum class ComponentDataType
+    {
+        Bool=0,
+        Float,
+        Int,
+        Uint,
+    };//
+
+    using ComponentBitsConfig=uint32;
+
+    constexpr ComponentBitsConfig MCC_PureColor     =uint32(ComponentBit::Color);
+    constexpr ComponentBitsConfig MCC_PureNormal    =uint32(ComponentBit::Normal);
+    constexpr ComponentBitsConfig MCC_ColorNormal   =uint32(ComponentBit::Color)|uint32(ComponentBit::Normal);
+    constexpr ComponentBitsConfig MCC_CNMR          =uint32(ComponentBit::Color)|uint32(ComponentBit::Normal)|uint32(ComponentBit::Metallic)|uint32(ComponentBit::Roughness);
+
+    struct ComponentConfig
+    {
+        Component           comp;               ///<成份ID
+        ComponentDataType   type;               ///<数据类型
+        uint                channels;           ///<通道数
+        bool                LinearColorspace;   ///<是要求线性颜色空间
+    };
+
+    const ComponentConfig *GetConfig(const enum class Component c);
+END_MATERIAL_NAMESPACE
+#endif//HGL_GRAPH_MATERIAL_COMPONENT_INCLUDE
---
+++ src/SceneGraph/MaterialComponent.cpp
@@ -0,0 +1,42 @@
+﻿#include<hgl/graph/MaterialComponent.h>
+
+BEGIN_MATERIAL_NAMESPACE
+    namespace
+    {
+        constexpr ComponentConfig material_component_config_list[]=
+        {
+#define MCC_DEFINE(name,dt,c,lc)    {Component::name,ComponentDataType::dt,c,lc}
+
+            MCC_DEFINE(ShadingModel,    Uint,   1,  false   ),
+
+            MCC_DEFINE(Color,           Float,  3,  true    ),
+
+            MCC_DEFINE(Opacity,         Float,  1,  false   ),
+            MCC_DEFINE(Normal,          Float,  3,  false   ),
+
+            MCC_DEFINE(Metallic,        Float,  1,  false   ),
+            MCC_DEFINE(Roughness,       Float,  1,  false   ),
+
+            MCC_DEFINE(Emissive,        Float,  3,  true    ),
+            MCC_DEFINE(Refraction,      Float,  1,  false   ),
+
+            MCC_DEFINE(AO,              Float,  1,  false   ),
+            MCC_DEFINE(SSS,             Float,  1,  false   ),
+            MCC_DEFINE(Height,          Float,  1,  false   ),
+
+            MCC_DEFINE(RIM,             Float,  1,  false   ),
+            MCC_DEFINE(ClearCoat,       Float,  1,  false   ),
+            MCC_DEFINE(Anisotropy,      Float,  1,  false   )
+
+#undef MCC_DEFINE
+        };
+    };//namespace
+
+    const ComponentConfig *GetConfig(const enum class Component c)
+    {
+        if(c<=Component::BEGIN_RANGE
+            ||c>=Component::END_RANGE)return(nullptr);
+
+        return material_component_config_list+(uint)c;
+    }
+END_MATERIAL_NAMESPACE
