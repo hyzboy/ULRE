056beaaa8ec8ec578d2843c5ea5d48333db5b75b
hyzboy <hyzboy@gmail.com>
2020-01-21 18:57:39 +0800

    add Subpass class

---
+++ inc/hgl/graph/vulkan/VKSubpass.h
@@ -0,0 +1,32 @@
+﻿#ifndef HGL_GRAPH_VULKAN_SUBPASS_INCLUDE
+#define HGL_GRAPH_VULKAN_SUBPASS_INCLUDE
+
+#include<hgl/graph/vulkan/VK.h>
+VK_NAMESPACE_BEGIN
+/**
+ * 渲染流程中一次具体的操作，即便整个Renderpass只有一次渲染，也需要创建subpass
+ */
+class Subpass
+{
+    VkSubpassDescription *desc;
+    VkSubpassDependency *dependency;
+
+    List<VkAttachmentReference> ar_input;
+    List<VkAttachmentReference> ar_out_colors;
+    VkAttachmentReference       ar_out_depth;
+
+public:
+
+    Subpass(VkSubpassDescription *sd,VkSubpassDependency *dep)
+    {
+        desc=sd;
+        dependency=dep;
+    }
+
+    virtual ~Subpass()=default;
+
+    const VkSubpassDescription *GetDescription  ()const{return desc;}
+    const VkSubpassDependency * GetDependency   ()const{return dependency;}
+};//class Subpass
+VK_NAMESPACE_END
+#endif//HGL_GRAPH_VULKAN_SUBPASS_INCLUDE
