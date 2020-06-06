98cb564aa5d9d58e94e7bcbae732a96f3186d272
hyzboy <hyzboy@gmail.com>
2020-06-06 19:45:38 +0800

    add ShaderResource.cpp/.h

---
+++ inc/hgl/graph/shader/ShaderResource.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<hgl/type/BaseString.h>
+
+namespace hgl
+{
+    namespace graph
+    {
+        class ShaderResource
+        {
+        public:
+        };//class ShaderResource
+
+        ShaderResource *LoadShaderResoruce(const OSString &filename);
+    }//namespace graph
+}//namespace hgl
