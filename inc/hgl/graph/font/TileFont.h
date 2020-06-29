54cd790c40a321f7b967bf20c6dd2f962dee42d5
hyzboy <hyzboy@gmail.com>
2020-06-29 19:49:54 +0800

    add TileFont.h

---
+++ inc/hgl/graph/font/TileFont.h
@@ -0,0 +1,22 @@
+#ifndef HGL_GRAPH_TILE_FONT_INCLUDE
+#define HGL_GRAPH_TILE_FONT_INCLUDE
+
+#include<hgl/graph/TileData.h>
+#include<hgl/graph/font/FontSource.h>
+
+namespace hgl
+{
+    namespace graph
+    {
+        class TileFont
+        {
+            FontSource *source;
+            TileData *tile_data;
+
+        public:
+
+            
+        };//class TileFont
+    }//namespace graph
+}//namespace hgl
+#endif//HGL_GRAPH_TILE_FONT_INCLUDE
