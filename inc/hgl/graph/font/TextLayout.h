a3745e927ddfc1c8d44070cad6ffe500b7ec7d98
hyzboy <hyzboy@gmail.com>
2020-07-20 21:29:15 +0800

    add TextLayout

--- inc/hgl/graph/font/TextLayout.h
+++ inc/hgl/graph/font/TextLayout.h
@@ -3,16 +3,12 @@
 
 #include<hgl/graph/font/FontSource.h>
 #include<hgl/graph/RenderableInstance.h>
-#include<hgl/graph/VertexBuffer.h>
+#include<hgl/graph/RenderableCreater.h>
+#include<hgl/type/RectScope.h>
 namespace hgl
 {
     namespace graph
     {
-
-        class TextRenderable:public vulkan::Renderable
-        {
-        };//class TextRenderable:public vulkan::Renderable
-
         /**
          * 文本排版处理
          */
@@ -21,11 +17,25 @@ namespace hgl
             FontSource *source;
 
         public:
+
+            enum class Direction
+            {
+                LeftToRight=0,          ///<横排从左到右
+                RightToLeft,            ///<横排从右到左
+                TopToRight,             ///<坚排从上到下从右到左
+            };//enum class Direction
+
+            RectScope2f scope;          ///<绘制区域
+
+            bool text_symbols;          ///<是否开启文本到符号自动替换
+            bool symbols_edge_disable;  ///<符号边界禁用
+
+        public:
         
             TextLayout(FontSource *fs):source(fs){}
             virtual ~TextLayout()=default;
 
-
+            bool Layout(const UTF16String &);
         };//class TextLayout
     }//namespace graph
 }//namespace hgl
