0dde26059b74278625d0a2e1373aec80b5d8ba2a
hyzboy <hyzboy@gmail.com>
2020-08-07 16:14:29 +0800

    use MemBlock instead uint8 * in WinBitmapFont

--- src/SceneGraph/font/FontSourceWin.h
+++ src/SceneGraph/font/FontSourceWin.h
@@ -2,32 +2,32 @@
 #define HGL_GRAPH_FONT_SOURCE_WINDOWS_INCLUDE
 
 #include<hgl/graph/font/FontSource.h>
+#include<hgl/type/MemBlock.h>
 #include<windows.h>
 
 namespace hgl
 {
-	namespace graph
-	{
-		class WinBitmapFont:public FontSourceSingle
-		{
-    		HDC hdc;
-			HFONT hfont;    		
-			
-			GLYPHMETRICS gm;
-			MAT2 mat;
+    namespace graph
+    {
+        class WinBitmapFont:public FontSourceSingle
+        {
+            HDC hdc;
+            HFONT hfont;    		
+            
+            GLYPHMETRICS gm;
+            MAT2 mat;
 
-			uint ggo;
+            uint ggo;
 
-			uint8 *buffer;
-			int buffer_size;
+            MemBlock<uint8> buffer;
 
-		public:
+        public:
 
-			WinBitmapFont(const Font &);
-			~WinBitmapFont();
+            WinBitmapFont(const Font &);
+            ~WinBitmapFont();
 
-			bool MakeCharBitmap(FontBitmap *,u32char) override;					///<产生字体数据
-		};//class WinBitmapFont
-	}//namespace graph
+            bool MakeCharBitmap(FontBitmap *,u32char) override;					///<产生字体数据
+        };//class WinBitmapFont
+    }//namespace graph
 }//namespace hgl
 #endif//HGL_GRAPH_FONT_SOURCE_WINDOWS_INCLUDE
