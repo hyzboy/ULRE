303c7150784bf9d02e759d22eebbf33b7cd114a6
hyzboy <hyzboy@gmail.com>
2020-09-20 21:17:16 +0800

    VulkanApplicationFramework add CreatePipeline functions override.

--- example/Vulkan/FragCoordTest.cpp
+++ example/Vulkan/FragCoordTest.cpp
@@ -32,11 +32,11 @@ private:
     vulkan::Material *          material            =nullptr;
     vulkan::MaterialInstance *  material_instance   =nullptr;
     vulkan::Renderable *        render_obj          =nullptr;
-    vulkan::Buffer *            ubo_world_matrix             =nullptr;
+    vulkan::Buffer *            ubo_world_matrix    =nullptr;
 
     vulkan::Pipeline *          pipeline            =nullptr;
 
-    vulkan::VAB *      vertex_buffer       =nullptr;
+    vulkan::VAB *               vertex_buffer       =nullptr;
 
 public:
 
