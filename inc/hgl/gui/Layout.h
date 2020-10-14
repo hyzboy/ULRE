111f1f89513990b192d7f82f00962d974db4ba83
hyzboy <hyzboy@gmail.com>
2020-10-14 21:05:30 +0800

    add gui/Layout.h Widget.h

---
+++ inc/hgl/gui/Layout.h
@@ -0,0 +1,58 @@
+#ifndef HGL_GUI_LAYOUT_INCLUDE
+#define HGL_GUI_LAYOUT_INCLUDE
+
+#include<hgl/gui/Widget.h>
+#include<hgl/type/Sets.h>
+namespace hgl
+{
+    namespace gui
+    {
+        /**
+         * 布局器基础
+         */
+        class LayoutBase
+        {
+        protected:
+
+            Sets<Widget *> widgets_set;
+
+        public:
+
+            virtual bool AddWidget(Widget *);
+            virtual bool RemoveWidget(Widget *);
+        };//class LayoutBase
+
+        /**
+         * 布局器
+         */
+        class Layout:public LayoutBase
+        {
+        public:
+        };//class Layout:public LayoutBase
+
+        /**
+         * 垂直分布布局器
+         */
+        class VBoxLayout:public LayoutBase
+        {
+        public:
+        };//class VBoxLayout:public LayoutBase
+
+        /**
+         * 水平分布布局器
+         */
+        class HBoxLayout:public LayoutBase
+        {
+        public:
+        };//class HBoxLayout:public LayoutBase
+
+        /**
+         * 网格分布布局器
+         */
+        class GridLayout:public LayoutBase
+        {
+        public:
+        };//class GridLayout:public LayoutBase
+    }//namespace gui
+}//namespace hgl
+#endif//HGL_GUI_LAYOUT_INCLUDE
