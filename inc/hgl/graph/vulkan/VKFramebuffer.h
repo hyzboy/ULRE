8d4b8a12a95f8ad91c25ace1c4a5e812490fb743
hyzboy <hyzboy@gmail.com>
2020-10-17 14:18:09 +0800

    Freambuffer deleted VKFramebufferCreateInfo data;

--- inc/hgl/graph/vulkan/VKFramebuffer.h
+++ inc/hgl/graph/vulkan/VKFramebuffer.h
@@ -7,36 +7,31 @@ class Framebuffer
 {
     VkDevice device;
     VkFramebuffer frame_buffer;
-    VkFramebufferCreateInfo *fb_info;
+    VkRenderPass render_pass;
 
     VkExtent2D extent;
+    uint32_t attachment_count;
     uint32_t color_count;
     bool has_depth;
-
-    ObjectList<Texture2D> color_texture;
-    Texture2D *depth_texture;
 
 private:
 
     friend class Device;
 
-    Framebuffer(VkDevice dev,VkFramebuffer fb,VkFramebufferCreateInfo *fb_create_info,bool depth);
+    Framebuffer(VkDevice,VkFramebuffer,const VkExtent2D &,VkRenderPass,uint32_t color_count,bool depth);
 
 public:
 
     ~Framebuffer();
 
     const   VkFramebuffer   GetFramebuffer      ()const{return frame_buffer;}
-    const   VkRenderPass    GetRenderPass       ()const{return fb_info->renderPass;}
+    const   VkRenderPass    GetRenderPass       ()const{return render_pass;}
 
     const   VkExtent2D &    GetExtent           ()const{return extent;}
 
-    const   uint32_t        GetAttachmentCount  ()const{return fb_info->attachmentCount;}           ///<获取渲染目标成分数量
+    const   uint32_t        GetAttachmentCount  ()const{return attachment_count;}                   ///<获取渲染目标成分数量
     const   uint32_t        GetColorCount       ()const{return color_count;}                        ///<取得颜色成分数量
     const   bool            HasDepth            ()const{return has_depth;}                          ///<是否包含深度成分
-
-            Texture2D *     GetColorTexture     (const int index=0){return color_texture[index];}
-            Texture2D *     GetDepthTexture     (){return depth_texture;}
 };//class Framebuffer
 VK_NAMESPACE_END
 #endif//HGL_GRAPH_VULKAN_FRAMEBUFFER_INCLUDE
