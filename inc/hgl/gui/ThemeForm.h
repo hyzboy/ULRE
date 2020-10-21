6dae81ac936f36254513dea584dd9a97ad2f978a
hyzboy <hyzboy@gmail.com>
2020-10-21 11:44:33 +0800

    add ThemeForm,DTForm

---
+++ inc/hgl/gui/ThemeForm.h
@@ -0,0 +1,26 @@
+#ifndef HGL_GUI_THEME_FORM_INCLUDE
+#define HGL_GUI_THEME_FORM_INCLUDE
+
+#include<hgl/graph/VKRenderTarget.h>
+#include<hgl/gui/Form.h>
+
+namespace hgl
+{
+    namespace gui
+    {
+        class ThemeForm
+        {
+        protected:
+        
+            Form *form;
+            hgl::graph::vulkan::RenderTarget *render_target;
+
+        public:
+
+            ThemeForm(Form *);
+
+            void SetRenderTarget(hgl::graph::vulkan::RenderTarget *);
+        };//class ThemeForm
+    }//namespace gui
+}//namespace hgl
+#endif//HGL_GUI_THEME_FORM_INCLUDE
