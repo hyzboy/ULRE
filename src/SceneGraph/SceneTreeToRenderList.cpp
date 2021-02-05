4108eaa342030bb62a8b6fb00797c83fdb3c0eee
hyzboy <hyzboy@gmail.com>
2021-02-05 17:15:16 +0800

    SceneTreeToRenderList

--- src/SceneGraph/SceneTreeToRenderList.cpp
+++ src/SceneGraph/SceneTreeToRenderList.cpp
@@ -1,15 +1,60 @@
-#include<hgl/graph/RenderList.h>
+﻿#include<hgl/graph/RenderList.h>
 
 namespace hgl
 {
     namespace graph
     {
+        SceneTreeToRenderList::~SceneTreeToRenderList()
+        {
+            SAFE_CLEAR(scene_node_list);
+        }
+
         uint32 SceneTreeToRenderList::CameraLength(SceneNode *,SceneNode *)
         {
         }
 
         bool SceneTreeToRenderList::InFrustum(const SceneNode *,void *)
         {
+        }
+
+        bool SceneTreeToRenderList::Begin()
+        {
+            if(!scene_node_list)
+                scene_node_list=new SceneNodeList;
+
+            scene_node_list->ClearData();
+
+            pipeline_sets.ClearData();
+            material_sets.ClearData();
+            mat_instance_sets.ClearData();
+
+            return(true);
+        }
+
+        /**
+        * 理论上讲，我们需要按以下顺序排序
+        *
+        * for(material)
+        *   for(pipeline)
+        *       for(material_instance)
+        *           for(vbo)
+        */
+
+        bool SceneTreeToRenderList::End()
+        {
+        }
+
+        bool SceneTreeToRenderList::Expend(SceneNode *sn)
+        {
+            if(!sn)return(false);
+
+            if(sn->renderable_instances.GetCount()>0)
+                scene_node_list->Add(sn);
+
+            for(SceneNode *sub:sn->SubNode)
+                Expend(sub);
+
+            return(true);
         }
 
         bool SceneTreeToRenderList::Expend(RenderList *rl,Camera *c,SceneNode *sn)
@@ -23,7 +68,11 @@ namespace hgl
 
             //Frustum f;
 
+            render_list=rl;
 
+            Begin();
+            Expend(sn);
+            End();
         }
     }//namespace graph
 }//namespace hgl
