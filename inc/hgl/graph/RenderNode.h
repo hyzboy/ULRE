ea6afd67f5bc643ecc0a95d17145448d6e10bad0
hyzboy <hyzboy@gmail.com>
2021-06-15 15:04:17 +0800

    added MVPMatrix attrib and GetUBOBytes function at RenderNode.

--- inc/hgl/graph/RenderNode.h
+++ inc/hgl/graph/RenderNode.h
@@ -1,8 +1,9 @@
-#ifndef HGL_GRAPH_RENDER_NODE_INCLUDE
+﻿#ifndef HGL_GRAPH_RENDER_NODE_INCLUDE
 #define HGL_GRAPH_RENDER_NODE_INCLUDE
 
 #include<hgl/math/Vector.h>
 #include<hgl/type/List.h>
+#include<hgl/graph/SceneInfo.h>
 namespace hgl
 {
     namespace graph
@@ -11,12 +12,21 @@ namespace hgl
 
         struct RenderNode
         {
+            MVPMatrix matrix;
+
             Vector4f WorldCenter;
 
             float distance_to_camera_square;
 //            float distance_to_camera;
 
             RenderableInstance *ri;
+
+        public:
+
+            /**
+             * 取得渲染对象ubo独占区大小
+             */
+            virtual const uint32 GetUBOBytes()const{return sizeof(MVPMatrix);}
         };//struct RenderNode
 
         using RenderNodeList=List<RenderNode *>;
