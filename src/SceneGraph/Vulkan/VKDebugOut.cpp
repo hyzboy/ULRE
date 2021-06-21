538b69cb45eb15be654471e72c168ee7a979a1e6
hyzboy <hyzboy@gmail.com>
2021-06-21 09:50:57 +0800

    fixed a bug, used a error messageType.

--- src/SceneGraph/Vulkan/VKDebugOut.cpp
+++ src/SceneGraph/Vulkan/VKDebugOut.cpp
@@ -205,7 +205,9 @@ bool VKDebugOut::Init(VkInstance i)
                                     |VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
                                     |VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
 
-        create_info.messageType     =VK_DEBUG_UTILS_MESSAGE_TYPE_FLAG_BITS_MAX_ENUM_EXT;
+        create_info.messageType     =VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
+                                    |VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
+                                    |VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
 
         create_info.pfnUserCallback =vulkan_debug_utils_callback;
         create_info.pUserData       =this;
