81e7b992c098e5ba237fe39d6c2a5506ed169327
hyzboy <hyzboy@gmail.com>
2021-12-15 19:57:35 +0800

    newly RenderTarget&SwapchainRenderTarget class

--- src/SceneGraph/Vulkan/VKSwapchainRenderTarget.cpp
+++ src/SceneGraph/Vulkan/VKSwapchainRenderTarget.cpp
@@ -3,8 +3,10 @@
 #include<hgl/graph/VKSemaphore.h>
 
 VK_NAMESPACE_BEGIN
-SwapchainRenderTarget::SwapchainRenderTarget(GPUDevice *dev,Swapchain *sc):RenderTarget(dev,nullptr,sc->GetImageCount())
+SwapchainRenderTarget::SwapchainRenderTarget(VkDevice dev,Swapchain *sc,GPUQueue *q,GPUSemaphore *rcs,GPUSemaphore *pcs,RenderPass *rp,Framebuffer **fbo_list):RenderTarget(q,rcs)
 {
+    device=dev;
+
     swapchain=sc;
     vk_swapchain=swapchain->GetSwapchain();
 
@@ -14,26 +16,17 @@ SwapchainRenderTarget::SwapchainRenderTarget(GPUDevice *dev,Swapchain *sc):Rende
     present_info.pResults           = nullptr;
     present_info.pSwapchains        = &vk_swapchain;
 
-    Texture2D **sc_color=swapchain->GetColorTextures();
-    Texture2D *sc_depth=swapchain->GetDepthTexture();
-
-    render_pass=dev->GetRenderPass();
+    render_pass=rp;
 
     swap_chain_count=swapchain->GetImageCount();
     
     extent=swapchain->GetExtent();
 
-    render_frame=new Framebuffer *[swap_chain_count];
-
-    for(uint i=0;i<swap_chain_count;i++)
-    {
-        render_frame[i]=device->CreateFramebuffer(render_pass,(*sc_color)->GetImageView(),sc_depth->GetImageView());
-        ++sc_color;
-    }
+    render_frame=fbo_list;
 
     current_frame=0;
 
-    present_complete_semaphore=dev->CreateGPUSemaphore();
+    present_complete_semaphore=pcs;
 }
 
 SwapchainRenderTarget::~SwapchainRenderTarget()
@@ -45,7 +38,7 @@ SwapchainRenderTarget::~SwapchainRenderTarget()
     
 int SwapchainRenderTarget::AcquireNextImage()
 {
-    if(vkAcquireNextImageKHR(device->GetDevice(),vk_swapchain,UINT64_MAX,*(this->present_complete_semaphore),VK_NULL_HANDLE,&current_frame)==VK_SUCCESS)
+    if(vkAcquireNextImageKHR(device,vk_swapchain,UINT64_MAX,*(this->present_complete_semaphore),VK_NULL_HANDLE,&current_frame)==VK_SUCCESS)
         return current_frame;
 
     return -1;
@@ -57,7 +50,7 @@ bool SwapchainRenderTarget::PresentBackbuffer(VkSemaphore *wait_semaphores,const
     present_info.pWaitSemaphores    =wait_semaphores;
     present_info.pImageIndices      =&current_frame;
 
-    VkResult result=vkQueuePresentKHR(queue,&present_info);
+    VkResult result=queue->Present(&present_info);
     
     if (!((result == VK_SUCCESS) || (result == VK_SUBOPTIMAL_KHR))) 
     {
@@ -80,11 +73,11 @@ bool SwapchainRenderTarget::PresentBackbuffer()
     
 bool SwapchainRenderTarget::Submit(VkCommandBuffer cb)
 {
-    return GPUQueue::Submit(cb,present_complete_semaphore,render_complete_semaphore);
+    return queue->Submit(cb,present_complete_semaphore,render_complete_semaphore);
 }
 
 bool SwapchainRenderTarget::Submit(VkCommandBuffer cb,GPUSemaphore *pce)
 {
-    return GPUQueue::Submit(cb,pce,render_complete_semaphore);
+    return queue->Submit(cb,pce,render_complete_semaphore);
 }
 VK_NAMESPACE_END
