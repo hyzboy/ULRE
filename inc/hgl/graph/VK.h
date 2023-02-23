014cd6435c918e210ddc07745d9558576375885a
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2023-02-23 12:31:42 +0800

    renamed to PerMaterialInstance instead of PerMaterial

--- inc/hgl/graph/VK.h
+++ inc/hgl/graph/VK.h
@@ -74,10 +74,10 @@ enum class DescriptorSetType
 {
     //设计使其对应shader中的set
     
-    Global=0,   ///<全局参数(如太阳光等)
-    PerFrame,   ///<帧参数(如摄像机等)
-    PerMaterial,///<材质参数(如纹理等)
-    PerObject,  ///<对象参数(如模型矩阵等)
+    Global=0,           ///<全局参数(如太阳光等)
+    PerFrame,           ///<帧参数(如摄像机等)
+    PerMaterialInstance,///<材质实例参数(如纹理等)
+    PerObject,          ///<对象参数(如模型矩阵等)
 
     Instance,
     Skeleton,
@@ -87,7 +87,7 @@ enum class DescriptorSetType
 
 constexpr char *DescriptSetsTypeName[]=
 {
-    "Global","PerFrame","PerMaterial","PerObject","Instance","Skeleton"
+    "Global","PerFrame","PerMaterialInstance","PerObject","Instance","Skeleton"
 };
 
 inline const char *GetDescriptorSetTypeName(const enum class DescriptorSetType &type)
