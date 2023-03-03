d54aff2a424af17977df49e6baf9d388ecc2a5db
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2023-03-03 23:02:40 +0800

    first added codes of ShaderGen

---
+++ inc/hgl/shadergen/MaterialDescriptorManager.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include<hgl/graph/VKShaderDescriptor.h>
+#include<hgl/graph/VKDescriptorSetType.h>
+#include<hgl/shadergen/ShaderCommon.h>
+#include<hgl/type/Map.h>
+
+using namespace hgl;
+using namespace hgl::graph;
+
+SHADERGEN_NAMESPACE_BEGIN
+/**
+* 材质描述符管理</p>
+* 该类使用于SHADER生成前，用于统计编号set/binding
+*/
+class MaterialDescriptorManager
+{
+    struct ShaderDescriptorSet
+    {
+        DescriptorSetType set_type;
+
+        int set;
+        int count;
+
+        ObjectMap<UTF8String,ShaderDescriptor>  descriptor_map;
+
+        List<const UBODescriptor *>             ubo_list;
+        List<const SamplerDescriptor *>         sampler_list;
+
+    public:
+
+        const ShaderDescriptor *AddDescriptor(VkShaderStageFlagBits ssb,ShaderDescriptor *new_sd);                       ///<添加一个描述符，如果它本身存在，则返回false
+    };
+
+    using ShaderDescriptorSetArray=ShaderDescriptorSet[size_t(DescriptorSetType::RANGE_SIZE)];
+
+    ShaderDescriptorSetArray desc_set_array;
+
+public:
+    
+    MaterialDescriptorManager();
+    ~MaterialDescriptorManager()=default;
+
+    const UBODescriptor *AddUBO(VkShaderStageFlagBits ssb,DescriptorSetType type,UBODescriptor *sd);
+    const SamplerDescriptor *AddSampler(VkShaderStageFlagBits ssb,DescriptorSetType type,SamplerDescriptor *sd);
+
+    const DescriptorSetType GetSetType(const AnsiString &)const;
+
+    void Resort();      //排序产生set号与binding号
+};
+SHADERGEN_NAMESPACE_END
---
+++ src/ShaderGen/ShaderDescriptorManager.cpp
@@ -0,0 +1,151 @@
+#include<hgl/shadergen/ShaderDescriptorManager.h>
+
+SHADERGEN_NAMESPACE_BEGIN
+ShaderDescriptorManager::ShaderDescriptorManager(VkShaderStageFlagBits cur,MaterialDescriptorManager *mdm)
+{
+    material_descriptor_manager=mdm;
+
+    stage_io.cur=cur;
+    stage_io.prev=(VkShaderStageFlagBits)0;
+    stage_io.next=(VkShaderStageFlagBits)0;
+    
+    hgl_zero(push_constant);
+}
+
+namespace
+{
+    bool Find(ObjectList<ShaderStage> &ol,const ShaderStage *ss)
+    {
+        for(int i=0;i<ol.GetCount();i++)
+            if(hgl::strcmp(ol[i]->name,ss->name)==0)
+                return(true);
+
+        return(false);
+    }
+}//namespace
+
+bool ShaderDescriptorManager::AddInput(ShaderStage *ss)
+{
+    if(!ss)return(false);
+
+    if(Find(stage_io.input,ss))return(false);
+
+    ss->location=stage_io.input.GetCount();
+    stage_io.input.Add(ss);
+    return(true);
+}
+
+bool ShaderDescriptorManager::AddOutput(ShaderStage *ss)
+{
+    if(!ss)return(false);
+
+    if(Find(stage_io.output,ss))return(false);
+    
+    ss->location=stage_io.output.GetCount();
+    stage_io.output.Add(ss);
+    return(true);
+}
+
+bool ShaderDescriptorManager::AddUBO(DescriptorSetType type,UBODescriptor *sd)
+{
+    const UBODescriptor *obj=material_descriptor_manager->AddUBO(stage_io.cur,type,sd);
+
+    if(!obj)
+        return(false);
+
+    ubo_list.Add(obj);
+    return obj;
+}
+
+bool ShaderDescriptorManager::AddSampler(DescriptorSetType type,SamplerDescriptor *sd)
+{
+    const SamplerDescriptor *obj=material_descriptor_manager->AddSampler(stage_io.cur,type,sd);
+
+    if(!obj)
+        return(false);
+
+    sampler_list.Add(obj);
+    return obj;
+}
+
+bool ShaderDescriptorManager::AddConstValue(ConstValueDescriptor *sd)
+{
+    if(!sd)return(false);
+
+    for(auto *p:const_value_list)
+        if(p->name.Comp(sd->name)==0)
+            return(false);
+
+    sd->constant_id=const_value_list.Add(sd);
+    return(true);
+}
+
+bool ShaderDescriptorManager::AddSubpassInput(const UTF8String name,uint8_t index)
+{
+    for(auto *si:subpass_input)
+    {
+        if(si->input_attachment_index==index)return(false);
+        if(si->name.Comp(name))return(false);
+    }
+
+    SubpassInputDescriptor *ssi=new SubpassInputDescriptor;
+
+    ssi->name=name;
+    ssi->input_attachment_index=index;
+
+    subpass_input.Add(ssi);
+    return(true);
+}
+
+void ShaderDescriptorManager::SetPushConstant(const UTF8String name,uint8_t offset,uint8_t size)
+{
+    push_constant.name  =name;
+    push_constant.offset=offset;
+    push_constant.size  =size;
+}
+
+#ifdef _DEBUG
+void ShaderDescriptorManager::DebugOutput(int index)
+{
+    UTF8String name=GetShaderStageName(stage_io.cur);
+    UTF8String prev_name=GetShaderStageName(stage_io.prev);
+    UTF8String next_name=GetShaderStageName(stage_io.next);
+    
+    LOG_INFO(UTF8String::numberOf(index)+": "+name+" shader");
+    LOG_INFO("\tprev next is "+prev_name);
+    LOG_INFO("\tnext next is "+next_name);
+
+    if(stage_io.input.GetCount()>0)
+    {
+        LOG_INFO("\tStage Input "+UTF8String::numberOf(stage_io.input.GetCount()));
+
+        for(auto *ss:stage_io.input)
+            LOG_INFO("\t\tlayout(location="+UTF8String::numberOf(ss->location)+") in "+UTF8String(GetShaderStageTypeName(ss))+"\t"+UTF8String(ss->name));
+    }
+
+    if(stage_io.output.GetCount()>0)
+    {
+        LOG_INFO("\tStage Output "+UTF8String::numberOf(stage_io.output.GetCount()));
+
+        for(auto *ss:stage_io.output)
+            LOG_INFO("\t\tlayout(location="+UTF8String::numberOf(ss->location)+") out "+UTF8String(GetShaderStageTypeName(ss))+"\t"+UTF8String(ss->name));
+    }
+    
+    if(ubo_list.GetCount()>0)
+    {
+        LOG_INFO("\tUBO "+UTF8String::numberOf(ubo_list.GetCount()));
+        
+        for(auto *ubo:ubo_list)
+            LOG_INFO("\t\tlayout(set="+UTF8String::numberOf(ubo->set)+",binding="+UTF8String::numberOf(ubo->binding)+") uniform "+ubo->type+"\t"+ubo->type);
+    }
+
+    if(sampler_list.GetCount()>0)
+    {
+        LOG_INFO("\tSampler "+UTF8String::numberOf(sampler_list.GetCount()));
+
+        for(auto *ubo:ubo_list)
+            LOG_INFO("\t\tlayout(set="+UTF8String::numberOf(ubo->set)+",binding="+UTF8String::numberOf(ubo->binding)+") uniform "+ubo->type+"\t"+ubo->type);
+    }
+}
+#endif//_DEBUG
+SHADERGEN_NAMESPACE_END
