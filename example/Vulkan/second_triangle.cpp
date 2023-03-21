718221ce0d2471c68f4a150d798e140ec7a94b84
hyzboy <hyzboy@gmail.com>
2023-03-22 02:41:19 +0800

    next work: support 3-way coordinate system in VertexColor2D material.

--- example/Vulkan/second_triangle.cpp
+++ example/Vulkan/second_triangle.cpp
@@ -46,7 +46,7 @@ private:
         if(!material_instance)
             return(false);
 
-        BindCameraUBO(material_instance);
+        db->BindGlobalDescriptor(material_instance);
             
 //        pipeline=db->CreatePipeline(material_instance,sc_render_target,OS_TEXT("res/pipeline/solid2d"));
         pipeline=CreatePipeline(material_instance,InlinePipeline::Solid2D,Prim::Triangles);     //等同上一行，为Framework重载，默认使用swapchain的render target
--- src/ShaderGen/2d/VertexColor2D.cpp
+++ src/ShaderGen/2d/VertexColor2D.cpp
@@ -1,14 +1,28 @@
 ﻿#include<hgl/graph/mtl/StdMaterial.h>
+#include<hgl/graph/mtl/2d/VertexColor2D.h>
 #include<hgl/graph/VKMaterial.h>
 #include<hgl/graph/VKDevice.h>
 #include<hgl/shadergen/MaterialCreateInfo.h>
 
 STD_MTL_NAMESPACE_BEGIN
-MaterialCreateInfo *CreateVertexColor2DNDC()
+MaterialCreateInfo *CreateVertexColor2D(const CoordinateSystem2D &cs)
 {
-    MaterialCreateInfo *mci=new MaterialCreateInfo("VertexColor2DNDC",      ///<名称，随便起
+    AnsiString mtl_name="VertexColor2D";
+
+    if(cs==CoordinateSystem2D::NDC)mtl_name+="NDC";else
+    if(cs==CoordinateSystem2D::ZeroToOne)mtl_name+="ZeroToOne";else
+    if(cs==CoordinateSystem2D::Ortho)mtl_name+="Ortho";else
+        return(nullptr);
+
+
+    MaterialCreateInfo *mci=new MaterialCreateInfo( mtl_name,               ///<名称
                                                     1,                      ///<最终一个RT输出
                                                     false);                 ///<无深度输出
+
+    if(cs==CoordinateSystem2D::Ortho)
+    {
+        mci->AddStruct(GlobalShaderUBO::ViewportInfo,"");
+    }
 
     //vertex部分
     {
