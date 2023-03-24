8e52595123646225d87f22a3e58b3ec4e5ab588f
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2023-03-24 22:14:43 +0800

    standalone RenderList2D

--- inc/hgl/graph/RenderList2D.h
+++ inc/hgl/graph/RenderList2D.h
@@ -1,37 +1,34 @@
-﻿#ifndef HGL_GRAPH_RENDER_LIST_INCLUDE
-#define HGL_GRAPH_RENDER_LIST_INCLUDE
+﻿#ifndef HGL_GRAPH_RENDER_LIST_2D_INCLUDE
+#define HGL_GRAPH_RENDER_LIST_2D_INCLUDE
 
 #include<hgl/graph/VK.h>
-#include<hgl/graph/Camera.h>
 #include<hgl/graph/SceneNode.h>
-#include<hgl/graph/RenderNode.h>
+#include<hgl/graph/RenderNode2D.h>
 #include<hgl/graph/VKArrayBuffer.h>
-#include<hgl/graph/SceneInfo.h>
-#include<hgl/color/Color4f.h>
-#include<hgl/type/SortedSets.h>
+#include<hgl/graph/VKMaterial.h>
 namespace hgl
 {
     namespace graph
     {
-        using MaterialSets=SortedSets<Material *>;
-
         /**
          * 渲染对象列表<br>
          * 已经展开的渲染对象列表，产生mvp用UBO/SSBO等数据，最终创建RenderCommandBuffer
          */
-        class RenderList
+        class RenderList2D
         {
         protected:  
 
-            GPUDevice *     device;
-            RenderCmdBuffer *cmd_buf;
+            GPUDevice *         device;
+            RenderCmdBuffer *   cmd_buf;        
 
         private:
 
-            RenderNodeList  render_node_list;       ///<场景节点列表
-            MaterialSets    material_sets;          ///<材质合集
+//            GPUArrayBuffer *    mvp_array;
 
-            RenderNodeComparator *render_node_comparator;
+            RenderNode2DList    render_node_list;       ///<场景节点列表
+            MaterialSets        material_sets;          ///<材质合集
+
+            RenderNode2DComparator render_node_comparator;
 
         private:
 
@@ -46,7 +43,7 @@ namespace hgl
         protected:
 
             virtual bool    Begin();
-            virtual bool    Expend(SceneNode *);
+            virtual bool    ExpendNode(SceneNode *);
             virtual void    End();
 
         private:
@@ -59,51 +56,13 @@ namespace hgl
 
         public:
 
-            RenderList(GPUDevice *);
-            virtual ~RenderList();
+            RenderList2D(GPUDevice *);
+            virtual ~RenderList2D();
             
-            virtual bool Expend(const CameraInfo &,SceneNode *);
+            virtual bool Expend(SceneNode *);
 
             virtual bool Render(RenderCmdBuffer *);
-        };//class RenderList
-
-        class RenderList2D:public RenderList
-        {
-
-        protected:
-
-            virtual bool    Begin() override;
-            virtual bool    Expend(SceneNode *) override;
-            virtual void    End() override;
-
-        public:
-
-            RenderList2D();
-            virtual ~RenderList2D() override;
-
-            virtual bool Expend(SceneNode *);
-        };
-
-        class RenderList3D:public RenderList
-        {
-        protected:
-
-            CameraInfo      camera_info;            
-            GPUArrayBuffer *mvp_array;
-
-        protected:
-
-            virtual bool    Begin() override;
-            virtual bool    Expend(SceneNode *) override;
-            virtual void    End() override;
-
-        public:
-
-            RenderList3D();
-            virtual ~RenderList3D() override;
-
-            bool Expend(const CameraInfo &,SceneNode *);
-        };
+        };//class RenderList2D
     }//namespace graph
 }//namespace hgl
-#endif//HGL_GRAPH_RENDER_LIST_INCLUDE
+#endif//HGL_GRAPH_RENDER_LIST_2D_INCLUDE
