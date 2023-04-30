1cccc6e606769af5fb57e08f31d540f703411c14
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2023-04-30 20:42:08 +0800

    moved VAN to CMSceneGraph from VertexAttribData

--- inc/hgl/graph/VKMaterialParameters.h
+++ inc/hgl/graph/VKMaterialParameters.h
@@ -40,7 +40,7 @@ public:
 
     #define MP_TYPE_IS(name)    const   bool is##name()const{return set_type==DescriptorSetType::name;}
         MP_TYPE_IS(Instance)
-        MP_TYPE_IS(PerMaterial)
+        //MP_TYPE_IS(PerMaterial)
         MP_TYPE_IS(PerFrame)
         MP_TYPE_IS(Global)
     #undef MP_TYPE_IS
