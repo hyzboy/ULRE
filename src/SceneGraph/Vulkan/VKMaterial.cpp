d5b69fbe71b2fac2144f3edb477fe0c7bc9f4f2b
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2023-05-12 21:36:52 +0800

    removed mi_data from Material

--- src/SceneGraph/Vulkan/VKMaterial.cpp
+++ src/SceneGraph/Vulkan/VKMaterial.cpp
@@ -16,15 +16,8 @@ MaterialData::~MaterialData()
     delete vertex_input;
 }
 
-Material::Material(MaterialData *md):data(md)
-{
-    mi_count=0;
-}
-
 Material::~Material()
 {
-    delete[] data->mi_data;
-
     delete data->pipeline_layout_data;
     delete data;
 }
