bf8e1e8d19c38fd42941daf3af4bc83848620b73
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-06-05 17:00:22 +0800

    removed source codes about "Load Material file"

--- src/SceneGraph/Vulkan/VKRenderResourceMaterial.cpp
+++ src/SceneGraph/Vulkan/VKRenderResourceMaterial.cpp
@@ -12,7 +12,6 @@
 #include<hgl/shadergen/ShaderDescriptorInfo.h>
 
 VK_NAMESPACE_BEGIN
-
 const ShaderModule *RenderResource::CreateShaderModule(const OSString &filename,VkShaderStageFlagBits shader_stage,const uint32_t *spv_data,const size_t spv_size)
 {
     if(!device)return(nullptr);
@@ -43,160 +42,6 @@ const ShaderModule *RenderResource::CreateShaderModule(const OSString &filename,
     #endif//_DEBUG
 
     return sm;
-}
-
-void LoadShaderDescriptor(io::ConstBufferReader &cbr,ShaderDescriptor *sd_list,const uint count,const uint8 ver)
-{
-    ShaderDescriptor *sd=sd_list;
-
-    for(uint i=0;i<count;i++)
-    {
-        cbr.CastRead<uint8>(sd->desc_type);
-        cbr.ReadTinyString(sd->name);
-
-        if(ver==3)
-            cbr.CastRead<uint8>(sd->set_type);
-        else
-        if(ver==2)      //以下是旧的，未来不用了，现仅保证能运行
-        {
-            if(sd->name[0]=='g')sd->set_type=DescriptorSetType::Global;else
-            //if(sd->name[0]=='m')sd->set_type=DescriptorSetType::PerMaterial;else
-            //if(sd->name[0]=='r')sd->set_type=DescriptorSetType::PerObject;else
-                sd->set_type=DescriptorSetType::PerMaterial;
-        }
-
-        cbr.Read(sd->set);
-        cbr.Read(sd->binding);
-        cbr.Read(sd->stage_flag);
-
-        //if(sd->set_type>=DescriptorSetType::PerObject)
-        //{
-        //    if(sd->desc_type==VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER)sd->desc_type=VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC;else
-        //    if(sd->desc_type==VK_DESCRIPTOR_TYPE_STORAGE_BUFFER)sd->desc_type=VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC;
-        //}
-
-        ++sd;
-    }
-}
-
-ShaderResource *LoadShaderResource(io::ConstBufferReader &cbr);
-
-Material *RenderResource::CreateMaterial(const OSString &filename)
-{
-    Material *mtl;
-
-    if(material_by_name.Get(filename,mtl))
-        return mtl;
-
-    constexpr char MaterialFileHeader[]=u8"Material\x1A";
-    constexpr uint MaterialFileHeaderLength=sizeof(MaterialFileHeader)-1;
-
-    int64 filesize;
-    uint8 *filedata=(uint8 *)filesystem::LoadFileToMemory(filename+OS_TEXT(".material"),filesize);
-
-    if(!filedata)
-    {
-        material_by_name.Add(filename,nullptr);
-        return(nullptr);
-    }
-
-    AutoDeleteArray<uint8> origin_filedata(filedata,filesize);
-
-    if(filesize<MaterialFileHeaderLength)
-        return(nullptr);
-
-    io::ConstBufferReader cbr(filedata,filesize);
-
-    if(memcmp(cbr.CurPointer(),MaterialFileHeader,MaterialFileHeaderLength)!=0)
-        return(nullptr);
-
-    cbr.Skip(MaterialFileHeaderLength);
-
-    uint8 ver;
-
-    cbr.Read(ver);
-
-    if(ver<2||ver>3)
-        return(nullptr);
-
-    uint32_t shader_bits;
-
-    cbr.Read(shader_bits);
-
-    const uint count=GetShaderCountByBits(shader_bits);
-    uint32_t size;
-
-    ShaderResource *sr;
-    const ShaderModule *sm;
-    
-    bool result=true;
-    ShaderModuleMap *smm=new ShaderModuleMap;
-    VertexInput *vertex_input=nullptr;
-
-    OSString shader_name;
-
-    for(uint i=0;i<count;i++)
-    {
-        cbr.Read(size);
-
-        sr=LoadShaderResource(io::ConstBufferReader(cbr,size));     //有一个stage output没读，放弃了，所以不能直接用上一级的cbr
-        cbr.Skip(size);
-
-        if(sr)
-        {
-            shader_name=filename+OS_TEXT("?")+ToOSString(sr->GetStageName());
-
-            sm=CreateShaderModule(shader_name,sr->GetStage(),sr->GetSPVData(),sr->GetSPVSize());
-
-            if(sm)
-            {
-                if(smm->Add(sm))
-                {
-                    if(sr->GetStage()==VK_SHADER_STAGE_VERTEX_BIT)
-                        vertex_input=new VertexInput(sr->GetInputs());
-
-                    continue;
-                }
-            }
-        }   
-    
-        result=false;
-        break;
-    }
-
-    const UTF8String mtl_name=ToUTF8String(filename);
-
-    MaterialDescriptorManager *desc_manager=nullptr;
-    {
-        uint8 count;
-        cbr.Read(count);
-
-        if(count>0)
-        {
-            ShaderDescriptor *sd_list=new ShaderDescriptor[count];
-
-            LoadShaderDescriptor(cbr,sd_list,count,ver);
-        
-            desc_manager=new MaterialDescriptorManager(mtl_name,sd_list,count);
-
-            delete[] sd_list;
-        }
-    }
-
-    if(result&&vertex_input)
-    {
-        mtl=device->CreateMaterial(mtl_name,smm,desc_manager,vertex_input);
-        Add(mtl);
-    }
-    else
-    {
-        SAFE_CLEAR(desc_manager);
-        delete smm;
-        mtl=nullptr;
-    }
-    
-    material_by_name.Add(filename,mtl);
-    return(mtl);
 }
 
 Material *RenderResource::CreateMaterial(const mtl::MaterialCreateInfo *mci)
@@ -276,6 +121,6 @@ Material *RenderResource::CreateMaterial(const mtl::MaterialCreateInfo *mci)
     }
 
     material_by_name.Add(mtl_name,mtl);
-    return mtl;    
+    return mtl;
 }
 VK_NAMESPACE_END
--- src/SceneGraph/Vulkan/VKShaderResource.cpp
+++ src/SceneGraph/Vulkan/VKShaderResource.cpp
@@ -5,39 +5,6 @@
 #include<hgl/io/ConstBufferReader.h>
 
 VK_NAMESPACE_BEGIN
-
-    namespace
-    {
-        ObjectMap<OSString,ShaderResource> shader_resource_by_filename;
-
-        const bool LoadShaderStageAttributes(ShaderAttributeArray &sa_array,io::ConstBufferReader &cbr)
-        {
-            uint count;
-
-            cbr.CastRead<uint8>(count);
-
-            if(count<=0)
-                return(false);
-
-            Init(&sa_array,count);
-
-            ShaderAttribute *ss=sa_array.items;
-
-            for(uint i=0;i<count;i++)
-            {
-                cbr.Read(ss->location);
-                cbr.CastRead<uint8>(ss->basetype);
-                cbr.CastRead<uint8>(ss->vec_size);
-
-                cbr.ReadTinyString(ss->name);
-
-                ++ss;
-            }
-
-            return true;
-        }
-    }//namespcae
-
     ShaderResource::ShaderResource(const VkShaderStageFlagBits &flag,const uint32_t *sd,const uint32 size)
     {
         stage_flag=flag;
@@ -84,23 +51,5 @@ VK_NAMESPACE_BEGIN
         }
 
         return -1;
-    }
-
-    ShaderResource *LoadShaderResource(io::ConstBufferReader &cbr)
-    {
-        VkShaderStageFlagBits   flag;
-        uint32                  spv_size;
-
-        cbr.CastRead<uint32>(flag);
-        cbr.Read(spv_size);
-
-        ShaderResource *sr=new ShaderResource(flag,(const uint32_t *)cbr.CurPointer(),spv_size);
-
-        cbr.Skip(spv_size);
-
-        LoadShaderStageAttributes(sr->GetInputs(),cbr);
-//        LoadShaderStageAttributes(sr->GetStageOutputs(),cbr);
-
-        return sr;
     }
 VK_NAMESPACE_END
