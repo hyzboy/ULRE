907cb3c852d4e0ce224a6af2b9df8a71683ff814
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-09-21 21:36:55 +0800

    added M_RectTexture2D.cpp

--- example/Vulkan/texture_rect.cpp
+++ example/Vulkan/texture_rect.cpp
@@ -1,10 +1,12 @@
-﻿// 2.texture_rect
-// 该示例是1.indices_rect的进化，演示在矩形上贴上贴图
+﻿// texture rect
+// 画一个带纹理的矩形，2D模式专用
 
 #include"VulkanAppFramework.h"
 #include<hgl/graph/VKTexture.h>
 #include<hgl/graph/VKSampler.h>
 #include<hgl/graph/VKInlinePipeline.h>
+#include<hgl/graph/VKRenderablePrimitiveCreater.h>
+#include<hgl/graph/mtl/2d/Material2DCreateConfig.h>
 #include<hgl/math/Math.h>
 
 using namespace hgl;
@@ -17,30 +19,16 @@ VK_NAMESPACE_END
 constexpr uint32_t SCREEN_WIDTH=256;
 constexpr uint32_t SCREEN_HEIGHT=256;
 
-constexpr uint32_t VERTEX_COUNT=4;
-
-constexpr float position_data[VERTEX_COUNT][2]=
+constexpr float position_data[4]=
 {
-    {0,             0},
-    {SCREEN_WIDTH,  0},
-    {0,             SCREEN_HEIGHT},
-    {SCREEN_WIDTH,  SCREEN_HEIGHT}
+    -1,-1,
+    2,2
 };
 
-constexpr float tex_coord_data[VERTEX_COUNT][2]=
+constexpr float tex_coord_data[4]=
 {
-    {0,0},
-    {1,0},
-    {0,1},
-    {1,1}
-};
-
-constexpr uint32_t INDEX_COUNT=6;
-
-constexpr uint16 index_data[INDEX_COUNT]=
-{
-    0,1,3,
-    0,3,2
+    0,0,
+    1,1
 };
 
 class TestApp:public VulkanApplicationFramework
@@ -50,21 +38,28 @@ private:
     Texture2D *         texture             =nullptr;
     Sampler *           sampler             =nullptr;
     MaterialInstance *  material_instance   =nullptr;
-    Renderable *        renderable          =nullptr;
+    Renderable *        render_obj          =nullptr;
     Pipeline *          pipeline            =nullptr;
 
 private:
 
     bool InitMaterial()
     {
-        material_instance=db->CreateMaterialInstance(OS_TEXT("res/material/Texture2D"));        
-        if(!material_instance)return(false);
+        mtl::Material2DCreateConfig cfg(device->GetDeviceAttribute(),"RectTexture2D");
 
-        BindCameraUBO(material_instance);
+        cfg.coordinate_system=CoordinateSystem2D::NDC;
+        cfg.local_to_world=false;
+
+        AutoDelete<mtl::MaterialCreateInfo> mci=mtl::CreateRectTexture2D(&cfg);
+
+        material_instance=db->CreateMaterialInstance(mci);
+
+        if(!material_instance)
+            return(false);
 
 //        pipeline=db->CreatePipeline(material_instance,sc_render_target,OS_TEXT("res/pipeline/solid2d"));
-        pipeline=CreatePipeline(material_instance,InlinePipeline::Solid2D,Prim::Triangles);     //等同上一行，为Framework重载，默认使用swapchain的render target
-        
+        pipeline=CreatePipeline(material_instance,InlinePipeline::Solid2D,Prim::SolidRectangles);     //等同上一行，为Framework重载，默认使用swapchain的render target
+
         if(!pipeline)
             return(false);
 
@@ -73,23 +68,24 @@ private:
 
         sampler=db->CreateSampler();
 
-        if(!material_instance->BindImageSampler(DescriptorSetType::Value,"tex",texture,sampler))return(false);
+        if(!material_instance->BindImageSampler(DescriptorSetType::PerMaterial,     ///<描述符合集
+                                                mtl::SamplerName::Color,            ///<采样器名称
+                                                texture,                            ///<纹理
+                                                sampler))                           ///<采样器
+            return(false);
 
         return(true);
     }
 
     bool InitVBO()
     {
-        auto primitive=db->CreatePrimitive(VERTEX_COUNT);
-        if(!primitive)return(false);
+        RenderablePrimitiveCreater rpc(db,1);
 
-        if(!primitive->Set(VAN::Position,db->CreateVBO(VF_V2F,VERTEX_COUNT,position_data)))return(false);
-        if(!primitive->Set(VAN::TexCoord,db->CreateVBO(VF_V2F,VERTEX_COUNT,tex_coord_data)))return(false);
-        if(!primitive->Set(db->CreateIBO16(INDEX_COUNT,index_data)))return(false);
+        if(!rpc.SetVBO(VAN::Position,VF_V4F,position_data))return(false);
+        if(!rpc.SetVBO(VAN::TexCoord,VF_V4F,tex_coord_data))return(false);
 
-        renderable=db->CreateRenderable(primitive,material_instance,pipeline);
-
-        return(true);
+        render_obj=rpc.Create(material_instance,pipeline);
+        return(render_obj);
     }
 
 public:
@@ -105,7 +101,7 @@ public:
         if(!InitVBO())
             return(false);
             
-        BuildCommandBuffer(renderable);
+        BuildCommandBuffer(render_obj);
 
         return(true);
     }
@@ -114,7 +110,7 @@ public:
     {
         VulkanApplicationFramework::Resize(w,h);
         
-        BuildCommandBuffer(renderable);
+        BuildCommandBuffer(render_obj);
     }
 };//class TestApp:public VulkanApplicationFramework
 
