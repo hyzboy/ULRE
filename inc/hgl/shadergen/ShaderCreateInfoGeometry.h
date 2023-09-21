0c071286990dbbef2b200ee8314b635b90363e17
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-09-21 21:36:37 +0800

    Added few codes of ShaderCreateInfoGeometry

--- inc/hgl/shadergen/ShaderCreateInfoGeometry.h
+++ inc/hgl/shadergen/ShaderCreateInfoGeometry.h
@@ -5,9 +5,15 @@
 namespace hgl{namespace graph{
 class ShaderCreateInfoGeometry:public ShaderCreateInfo
 {
+    AnsiString input_prim;
+    AnsiString output_prim;
+    uint32_t max_vertices;    
+
 public:
 
     ShaderCreateInfoGeometry(MaterialDescriptorInfo *m):ShaderCreateInfo(VK_SHADER_STAGE_GEOMETRY_BIT,m){}
     ~ShaderCreateInfoGeometry()=default;
+
+    bool SetGeom(const Prim &ip,const Prim &op,const uint32_t mv);
 };
 }}//namespace hgl::graph
