ec558e157033d15999fb9b12cb68dd50ccc8c885
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-09-25 15:37:41 +0800

    improved codes.

--- src/SceneGraph/Vulkan/Texture/CopyBufferToImage.h
+++ src/SceneGraph/Vulkan/Texture/CopyBufferToImage.h
@@ -11,50 +11,5 @@ struct CopyBufferToImageInfo
 
     const VkBufferImageCopy *   bic_list;
     uint32_t                    bic_count;
-
-public:
-
-    CopyBufferToImageInfo()
-    {
-        hgl_zero(*this);
-    }
-
-    CopyBufferToImageInfo(Texture *tex,VkBuffer buf,const VkBufferImageCopy *bic)
-    {
-        image               =tex->GetImage();
-        buffer              =buf;
-
-        isr.aspectMask      =tex->GetAspect();
-
-        isr.baseMipLevel    =0;
-        isr.levelCount      =tex->GetMipLevel();
-
-        isr.baseArrayLayer  =0;
-        isr.layerCount      =1;
-
-        bic_list            =bic;
-        bic_count           =1;
-    }
 };
-
-struct CopyBufferToImageMipmapsInfo:public CopyBufferToImageInfo
-{
-    CopyBufferToImageMipmapsInfo(Texture2D *tex,VkBuffer buf,const VkBufferImageCopy *bic,const uint32_t miplevel)
-    {
-        image               =tex->GetImage();
-        buffer              =buf;
-
-        isr.aspectMask      =tex->GetAspect();
-
-        isr.baseMipLevel    =0;
-        isr.levelCount      =tex->GetMipLevel();
-
-        isr.baseArrayLayer  =0;
-        isr.layerCount      =1;
-
-        bic_list            =bic;
-        bic_count           =miplevel;
-    }
-};
-
 VK_NAMESPACE_END
