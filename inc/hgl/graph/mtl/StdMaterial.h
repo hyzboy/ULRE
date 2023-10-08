0524c51feb66317356869f734f3402493574a694
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-10-08 10:04:58 +0800

    Set Base-class StdMaterial to Std2DMaterial/Std3DMaterial

--- inc/hgl/graph/mtl/StdMaterial.h
+++ inc/hgl/graph/mtl/StdMaterial.h
@@ -10,16 +10,41 @@
 
 #define STD_MTL_FUNC NAMESPACE_USING using namespace hgl::graph::mtl::func;
 
-
 namespace hgl
 {
     namespace graph
     {
+        class ShaderCreateInfoVertex;
+        class ShaderCreateInfoGeometry;
+        class ShaderCreateInfoFragment;
+
         namespace mtl
         {
             namespace func
             {
             }//namespace func
+
+            class MaterialCreateInfo;
+
+            class StdMaterial
+            {
+            protected:
+
+                virtual bool BeginCustomShader(){return true;/*some work before creating shader*/};
+
+                virtual bool CustomVertexShader(ShaderCreateInfoVertex *)=0;
+                virtual bool CustomGeometryShader(ShaderCreateInfoGeometry *){return false;}
+                virtual bool CustomFragmentShader(ShaderCreateInfoFragment *)=0;
+
+                virtual bool EndCustomShader(){return true;/*some work after creating shader*/};
+
+            public:
+
+                StdMaterial()=default;
+                virtual ~StdMaterial()=default;
+    
+                virtual MaterialCreateInfo *Create()=0;
+            };//class StdMaterial
         }//namespace mtl
     }//namespace graph
 }//namespace hgl
