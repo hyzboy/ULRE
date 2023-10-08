237c1a469b7332eb23b21a041639925423eaf948
hyzboy <hyzboy@gmail.com>
2023-10-09 03:15:42 +0800

    writed GeometryShaderStateParse, need to test in furture.

--- src/ShaderGen/ShaderCreateInfoGeometry.cpp
+++ src/ShaderGen/ShaderCreateInfoGeometry.cpp
@@ -6,20 +6,14 @@ namespace hgl
     {
         bool ShaderCreateInfoGeometry::SetGeom(const Prim &ip,const Prim &op,const uint32_t mv)
         {
-            if(ip==Prim::Points         )input_prim="points";else
-            if(ip==Prim::Lines          )input_prim="lines";else
-            if(ip==Prim::LinesAdj       )input_prim="lines_adjacency";else
-            if(ip==Prim::Triangles      )input_prim="triangles";else
-            if(ip==Prim::TrianglesAdj   )input_prim="triangles_adjacency";else
-                return(false);
-
-            if(op==Prim::Points         )output_prim="points";else
-            if(op==Prim::LineStrip      )output_prim="line_strip";else
-            if(op==Prim::TriangleStrip  )output_prim="triangle_strip";else
-                return(false);
+            if(!CheckGeometryShaderIn(ip))return(false);
+            if(!CheckGeometryShaderOut(op))return(false);
 
             if(mv==0)
                 return(false);
+
+            input_prim=GetPrimName(ip);
+            output_prim=GetPrimName(op);
 
             max_vertices=mv;
             return(true);
