ba2d479f053846a6fd0954061bcba4d0e513c8ff
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-11-02 18:51:02 +0800

    improved Gizmo3DMove.cpp

---
+++ example/Gizmo/Gizmo.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include<hgl/graph/VK.h>
+
+VK_NAMESPACE_BEGIN
+
+class Gizmo
+{
+    Material *          material;
+    MaterialInstance *  mi;
+    Pipeline *          pipeline;
+    Primitive *         primitive;
+
+};//class Gizmo
+
+VK_NAMESPACE_END
