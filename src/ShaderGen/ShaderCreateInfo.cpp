c8b3ed0286d1a48a00cc5836891229b0d0d36009
hyzboy <hyzboy@gmail.com>
2024-03-07 13:59:28 +0800

    preparing blinnphong light sample. added codes of to load ubo.

--- src/ShaderGen/ShaderCreateInfo.cpp
+++ src/ShaderGen/ShaderCreateInfo.cpp
@@ -1,4 +1,4 @@
-#include<hgl/shadergen/ShaderCreateInfo.h>
+﻿#include<hgl/shadergen/ShaderCreateInfo.h>
 #include<hgl/shadergen/ShaderDescriptorInfo.h>
 #include<hgl/graph/mtl/UBOCommon.h>
 #include<hgl/log/LogInfo.h>
@@ -448,6 +448,7 @@ bool ShaderCreateInfo::CreateShader(ShaderCreateInfo *last_sc)
 
 #ifdef _DEBUG
 
+    //想办法存成文件或是输出行号，以方便出错了调试
     LOG_INFO(AnsiString(GetShaderStageName(shader_stage))+" shader: \n"+final_shader);
 
 #endif//_DEBUG
