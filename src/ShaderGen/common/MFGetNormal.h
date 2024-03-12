eab3bc927afd215183fbb6f807bf88c349839b97
hyzboy <hyzboy@gmail.com>
2024-03-12 23:04:12 +0800

    resume GetNormalMatrix/GetNormal in auto shader.

--- src/ShaderGen/common/MFGetNormal.h
+++ src/ShaderGen/common/MFGetNormal.h
@@ -18,5 +18,12 @@ vec3 GetNormal(mat3 normal_matrix,vec3 normal)
     return normalize(normal_matrix*normal);
 }
 )";
+
+    constexpr const char *GetNormalVS=R"(
+vec3 GetNormal()
+{
+    return normalize(mat3(camera.view*GetLocalToWorld())*Normal);
+}
+)";
 }//namespace func
 STD_MTL_NAMESPACE_END
