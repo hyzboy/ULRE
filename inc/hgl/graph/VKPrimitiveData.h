d2684c3d1e473035d16083935427cf6ad522f9a6
hyzboy <hyzboy@gmail.com>
2024-04-26 01:17:47 +0800

    added VKPrimitiveData.h

---
+++ inc/hgl/graph/VKPrimitiveData.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include<hgl/graph/VKVertexAttribBuffer.h>
+#include<hgl/graph/VKIndexBuffer.h>
+#include<hgl/graph/AABB.h>
+
+VK_NAMESPACE_BEGIN
+
+struct PrimitiveData
+{
+    uint32_t vertex_count;
+
+    uint32_t va_count;
+
+    VABAccess *vab_list;
+
+    IndexBufferAccess ib_access;
+
+    AABB BoundingBox;
+};
+
+VK_NAMESPACE_END
