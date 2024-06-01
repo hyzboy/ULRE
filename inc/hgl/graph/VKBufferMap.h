993a0619b054b2d6291e6e25fd0eb5629fa60098
hyzboy <hyzboy@gmail.com>
2024-06-01 12:08:49 +0800

    added VKBufferMap.cpp/.h

---
+++ inc/hgl/graph/VKBufferMap.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include<hgl/graph/VK.h>
+#include<hgl/graph/VertexAttribDataAccess.h>
+
+VK_NAMESPACE_BEGIN
+class VKBufferMap
+{
+protected:
+
+    DeviceBuffer *buffer;
+    VkDeviceSize offset;
+    VkDeviceSize size;
+
+    void *map_ptr;
+
+public:
+
+    VKBufferMap(DeviceBuffer *buf_ptr,VkDeviceSize off,VkDeviceSize s);
+    virtual ~VKBufferMap();
+
+    const bool IsValid()const{ return buffer; }
+
+    void *Map();
+    void Unmap();
+};//class VKBufferMap
+VK_NAMESPACE_END
---
+++ src/SceneGraph/Vulkan/VKBufferMap.cpp
@@ -0,0 +1,43 @@
+﻿#include<hgl/graph/VKBufferMap.h>
+#include<hgl/graph/VKVertexAttribBuffer.h>
+
+VK_NAMESPACE_BEGIN
+
+VKBufferMap::VKBufferMap(DeviceBuffer *buf,VkDeviceSize off,VkDeviceSize s)
+{
+    buffer=buf;
+    offset=off;
+    size=s;
+
+    map_ptr=nullptr;
+}
+
+VKBufferMap::~VKBufferMap()
+{
+    if(map_ptr)
+        buffer->DeviceBuffer::Unmap();
+}
+
+void *VKBufferMap::Map()
+{
+    if(map_ptr)
+        return(map_ptr);
+
+    if(!buffer)
+        return(nullptr);
+
+    map_ptr=buffer->DeviceBuffer::Map(offset,size);
+    return map_ptr;
+
+}
+
+void VKBufferMap::Unmap()
+{
+    if(map_ptr)
+    {
+        buffer->DeviceBuffer::Unmap();
+        map_ptr=nullptr;
+    }
+}
+
+VK_NAMESPACE_END
