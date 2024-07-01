94a0e03c44ef2367e6086c3da3d4f3ff78d8aa96
hyzboy <hyzboy@gmail.com>
2024-07-02 00:51:59 +0800

    [WIP] MIPManager

---
+++ inc/hgl/graph/MIPManager.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include<hgl/graph/VK.h>
+
+VK_NAMESPACE_BEGIN
+
+class RenderResource;
+
+/**
+* <材质、材质实例、管线>资源管理
+*/
+class MIP
+{
+protected:
+
+    RenderResource *rr;
+
+    Material *material;
+
+public:
+
+
+};
+
+VK_NAMESPACE_END
