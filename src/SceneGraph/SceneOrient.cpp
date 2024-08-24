410b94a1276086a84b2d985ea9d35843cba85e82
hyzboy <hyzboy@gmail.com>
2024-08-25 04:03:57 +0800

    [WIP] improving SceneOrient

--- src/SceneGraph/SceneOrient.cpp
+++ src/SceneGraph/SceneOrient.cpp
@@ -5,60 +5,88 @@ namespace hgl
     {
         SceneOrient::SceneOrient()
         {
-            LocalMatrix=Identity4f;
+            parent_matrix=Identity4f;
+            local_matrix=Identity4f;
 
             SetWorldMatrix(Identity4f);
+
+            parent_matrix_dirty=false;
+            local_matrix_dirty=false;
+            transform_version=transform_manager.GetCurrentVersion();
         }
         
         SceneOrient::SceneOrient(const SceneOrient &so)
         {
-            hgl_cpy(*this,so);
+        #define SO_COPY(value)  value=so.value;
+
+            SO_COPY(parent_matrix)
+            SO_COPY(parent_matrix_dirty)
+
+            SO_COPY(local_matrix)
+            SO_COPY(local_matrix_dirty)
+
+            SO_COPY(transform_manager)
+            SO_COPY(transform_version)
+
+            SO_COPY(local_to_world_matrix)
+            SO_COPY(inverse_local_to_world_matrix)
+            SO_COPY(inverse_transpose_local_to_world_matrix)
+
+        #undef SO_COPY
         }
 
-        SceneOrient::SceneOrient(const Matrix4f &mat)
+        SceneOrient::SceneOrient(const Matrix4f &mat):SceneOrient()
         {
             SetLocalMatrix(mat);
         }
 
         void SceneOrient::SetLocalMatrix(const Matrix4f &mat)
         {
-            if(IsNearlyEqual(LocalMatrix,mat))
+            if(IsNearlyEqual(local_matrix,mat))
                 return;
 
-            LocalMatrix=mat;
+            local_matrix=mat;
         }
 
         void SceneOrient::SetWorldMatrix(const Matrix4f &wm)
         {
-            LocalToWorldMatrix                  =wm;
-            InverseLocalToWorldMatrix           =inverse(LocalToWorldMatrix);
-            InverseTransposeLocalToWorldMatrix  =transpose(InverseLocalToWorldMatrix);
+            local_to_world_matrix                  =wm;
+            inverse_local_to_world_matrix          =inverse(local_to_world_matrix);
+            inverse_transpose_local_to_world_matrix=transpose(inverse_local_to_world_matrix);
         }
 
         /**
          * 刷新世界变换矩阵
          * @param parent_matrix 上一级LocalToWorld变换矩阵
          */
-        bool SceneOrient::RefreshMatrix(const Matrix4f &parent_matrix)
+        bool SceneOrient::RefreshMatrix(const Matrix4f &pm)
         {
-            if(hgl_cmp(ParentMatrix,parent_matrix)==0)      //如果上一级到这一级没变，自然是可以用memcmp比较的
+            if(hgl_cmp(pm,parent_matrix)==0)      //如果上一级到这一级没变，自然是可以用memcmp比较的
                 return(true);
 
-            ParentMatrix=parent_matrix;
+            parent_matrix=pm;
 
-            if(IsIdentityMatrix(LocalMatrix))
-            {
-                SetWorldMatrix(parent_matrix);
-            }
-            else
-            if(IsIdentityMatrix(parent_matrix))
-            {
-                SetWorldMatrix(LocalMatrix);
-            }
-            else
-            {
-                SetWorldMatrix(MatrixMult(parent_matrix,LocalMatrix));
-            }
+            Matrix4f tm;
+            
+            transform_manager.GetMatrix(tm);
+
+            SetWorldMatrix(parent_matrix*local_matrix*tm);
+
+            //if(IsIdentityMatrix(LocalMatrix))
+            //{
+            //    SetWorldMatrix(parent_matrix);
+            //    return(true);
+            //}
+            //
+
+            //if(IsIdentityMatrix(parent_matrix))
+            //{
+            //    SetWorldMatrix(LocalMatrix);
+            //}
+            //else
+            //{
+            //    SetWorldMatrix(parent_matrix*LocalMatrix);
+            //}
 
             return(true);
         }
