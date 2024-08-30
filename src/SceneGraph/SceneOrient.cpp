8d5beec36a1d0ab2a49d81cc96a5b310b5294369
hyzboy <hyzboy@gmail.com>
2024-08-31 00:04:20 +0800

    Moved WorldPosition to SceneMatrix

--- src/SceneGraph/SceneOrient.cpp
+++ src/SceneGraph/SceneOrient.cpp
@@ -6,7 +6,6 @@ namespace hgl
         SceneOrient::SceneOrient(const SceneOrient &so)
         {
             scene_matrix=so.scene_matrix;
-            WorldPosition=so.WorldPosition;
 
             scene_matrix.UpdateNewestData();
         }
@@ -16,8 +15,6 @@ namespace hgl
             scene_matrix.SetLocalMatrix(mat);
             
             scene_matrix.UpdateNewestData();
-
-            WorldPosition=TransformPosition(GetLocalToWorldMatrix(),ZeroVector3f);
         }
 
         void SceneOrient::RefreshMatrix()
@@ -27,8 +24,6 @@ namespace hgl
                 //是最新版本，证明没有更新，那不用刷新了
                 return;
             }
-
-            WorldPosition=TransformPosition(GetLocalToWorldMatrix(),ZeroVector3f);
         }
     }//namespace graph
 }//namespace hgl
