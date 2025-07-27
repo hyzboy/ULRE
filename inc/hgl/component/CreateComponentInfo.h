510168e91251f44cfccc077103e7cb2f82c20f77
hyzboy <hyzboy@gmail.com>
2025-07-27 16:46:35 +0800

    CreateComponentInfo代码排版

--- inc/hgl/component/CreateComponentInfo.h
+++ inc/hgl/component/CreateComponentInfo.h
@@ -2,49 +2,45 @@
 
 #include<hgl/math/Matrix.h>
 
-namespace hgl
+namespace hgl::graph
 {
-    namespace graph
+    class SceneNode;
+
+    struct CreateComponentInfo
     {
-        class SceneNode;
+        graph::SceneNode *owner_node;           ///<所属节点
+        graph::Matrix4f mat;                    ///<变换矩阵
 
-        struct CreateComponentInfo
+    public:
+
+        CreateComponentInfo()
         {
-            graph::SceneNode *owner_node;           ///<所属节点
-            graph::Matrix4f mat;                    ///<变换矩阵
+            owner_node=nullptr;
+            mat=graph::Identity4f;
+        }
 
-        public:
+        CreateComponentInfo(const CreateComponentInfo &cci)
+        {
+            owner_node=cci.owner_node;
+            mat=cci.mat;
+        }
 
-            CreateComponentInfo()
-            {
-                owner_node=nullptr;
-                mat=graph::Identity4f;
-            }
+        CreateComponentInfo(graph::SceneNode *pn,const graph::Matrix4f &m)
+        {
+            owner_node=pn;
+            mat=m;
+        }
 
-            CreateComponentInfo(const CreateComponentInfo &cci)
-            {
-                owner_node=cci.owner_node;
-                mat=cci.mat;
-            }
+        CreateComponentInfo(graph::SceneNode *pn)
+        {
+            owner_node=pn;
+            mat=graph::Identity4f;
+        }
 
-            CreateComponentInfo(graph::SceneNode *pn,const graph::Matrix4f &m)
-            {
-                owner_node=pn;
-                mat=m;
-            }
-
-            CreateComponentInfo(graph::SceneNode *pn)
-            {
-                owner_node=pn;
-                mat=graph::Identity4f;
-            }
-
-            CreateComponentInfo(const graph::Matrix4f &m)
-            {
-                owner_node=nullptr;
-                mat=m;
-            }
-        };//struct CreateComponentInfo
-
-    }//namespace graph
-}//namespace hgl
+        CreateComponentInfo(const graph::Matrix4f &m)
+        {
+            owner_node=nullptr;
+            mat=m;
+        }
+    };//struct CreateComponentInfo
+}//namespace hgl::graph
