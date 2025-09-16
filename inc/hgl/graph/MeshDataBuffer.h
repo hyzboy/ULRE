e2a8cc074a1c86ae3078bc59f7572062e2e2b8d9
hyzboy <hyzboy@gmail.com>
2025-09-16 13:21:00 +0800

    拆分MeshDataBuffer/MeshRenderData到独立.h

---
+++ inc/hgl/graph/MeshDataBuffer.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include<hgl/graph/VK.h>
+
+VK_NAMESPACE_BEGIN
+/**
+* 原始图元数据缓冲区<Br>
+* 提供在渲染之前的数据绑定信息
+*/
+struct MeshDataBuffer:public Comparator<MeshDataBuffer>
+{
+    uint32_t        vab_count;
+    VkBuffer *      vab_list;
+
+    // 理论上讲，每个VAB绑定时都是可以指定byte offsets的。但是随后Draw时，又可以指定vertexOffset。
+    // 在我们支持的两种draw模式中，一种是每个模型一批VAB，所有VAB的offset都是0。
+    // 另一种是使用VDM，为了批量渲染，所有的VAB又必须对齐，所以每个VAB单独指定offset也不可行。
+
+    VkDeviceSize *  vab_offset;         //注意：这里的offset是字节偏移，不是顶点偏移
+
+    IndexBuffer *   ibo;
+
+    VertexDataManager *vdm;             //只是用来区分和比较的，不实际使用
+
+public:
+
+    MeshDataBuffer(const uint32_t,IndexBuffer *,VertexDataManager *_v=nullptr);
+    ~MeshDataBuffer();
+
+    const int compare(const MeshDataBuffer &mesh_data_buffer)const override;
+
+    bool Update(const Primitive *,const VIL *);
+};//struct MeshDataBuffer
+VK_NAMESPACE_END
