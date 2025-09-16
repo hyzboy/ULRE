94de7053d7e02cf87d9e0c062b835562848935ec
hyzboy <hyzboy@gmail.com>
2025-09-16 13:23:54 +0800

    [WIP]DrawLineManager开发中

--- example/Geometry/DrawLineManager.h
+++ example/Geometry/DrawLineManager.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <cstdint>
 #include <vector>
@@ -11,63 +11,70 @@
 #include<hgl/math/Vector.h>
 #include<hgl/color/Color4f.h>
 #include<hgl/type/String.h>
+#include<hgl/graph/VKBuffer.h>
+#include<hgl/graph/VKVertexAttribBuffer.h>
 #include<hgl/graph/VKCommandBuffer.h>
-
-// Forward declarations for GPU primitive creation
-namespace hgl
-{
-    namespace graph
-    {
-        class VulkanDevice;
-        class Primitive;
-        class VIL;
-        class Material;
-        class MaterialInstance;
-        class BufferManager;
-        class PrimitiveManager;
-        class MeshManager;
-        class Pipeline;
-        class MaterialManager;
-    }
-}
 
 namespace hgl::graph
 {
     constexpr const size_t MAX_LINE_WIDTH = 16;
 
+    struct LineSegmentInfo
+    {
+        Vector3f from;
+        Vector3f to;
+
+        uint8 color;
+    };
+
+    using LineColorPattle=Color4f[256];
+
+    using UBOLineColorPattle=UBOInstance<LineColorPattle>;
+
     class DrawLineManager
     {
         struct LineSegmentBuffer
         {
-            VulkanDevice *device;
-            MaterialInstance *mtl_inst;
-            Pipeline *pipeline;
+            VulkanDevice *      device  =nullptr;
+            MaterialInstance *  mtl_inst=nullptr;
+            Pipeline *          pipeline=nullptr;
 
-            uint width;
+            uint width=0;
 
-            std::vector<float> position;
-            std::vector<uint8> color;
+            uint32 max_count=0;     // 当前缓冲区可容纳的最大线段数量
+            uint32 count    =0;     // 当前线段数量
 
-            uint32 max_count;       // 当前缓冲区可容纳的最大线段数量
-            uint32 count;           // 当前线段数量
+            Primitive * primitive   =nullptr;
+            Mesh *      mesh        =nullptr;
+            VABList *   vab_list    =nullptr;
+            VABMap3f *  vab_position=nullptr;
+            VABMap1u8 * vab_color   =nullptr;
 
-            Primitive *primitive;
-            Mesh *mesh;
+            VB3f *      position    =nullptr;   // 每条线段2个顶点，每个顶点3个float
+            VB1u8 *     color       =nullptr;   // 每条线段2个顶点，每个顶点4个uint8
+
+            bool        dirty       =true;
 
         public:
 
-            LineSegmentBuffer(const uint w,VulkanDevice *,MaterialInstance *,Pipeline *p);
             ~LineSegmentBuffer();
 
+            void Init(const uint w,VulkanDevice *,MaterialInstance *,Pipeline *p);
             void Clear();
             bool RebuildMesh();
+            void AddCount(uint);
+
+            void AddLine(const Vector3f &from,const Vector3f &to,uint8_t color_index);
+            void AddLine(const DataArray<LineSegmentInfo> &);
+
+            void Update();
+
+            void Draw(RenderCmdBuffer *);
         };
 
     private:
 
-        Color4f color_palette[256] = {};
-
-        bool color_dirty = true;
+        UBOLineColorPattle *ubo_color;
 
     private:
 
@@ -77,33 +84,27 @@ namespace hgl::graph
 
     private:    
 
-        RenderTarget *      render_target       = nullptr;
         MaterialInstance *  mi_line3d           = nullptr;
         Pipeline *          pipeline            = nullptr;
         MeshManager *       mesh_manager        = nullptr;
-
 
         void UpdateLines();
 
     public:
 
-        DrawLineManager(RenderTarget *rt,
-                        MaterialInstance *mi,
+        DrawLineManager(MaterialInstance *mi,
+                        Pipeline *p,
+                        UBOLineColorPattle *lcp,
                         MeshManager *mm);
 
-        ~DrawLineManager() { ClearLines(); }
+        ~DrawLineManager();
 
-        void SetColor(const int index, const Color4f& c)
-        {
-            if (index < 0 || index >= 256) return;
-
-            color_palette[index] = c;
-        }
-
-        void ClearColor(){color_dirty = true;}
+        void SetColor(const int index, const Color4f& c);
 
         // Now specify colors by an index into the internal palette
         bool AddLine(const Vector3f& from, const Vector3f& to, const uint8_t color_index, uint8_t width = 1);
+
+        bool AddLine(const uint8_t width,const DataArray<LineSegmentInfo> &);
         void ClearLines();
 
         bool Draw(RenderCmdBuffer *);
