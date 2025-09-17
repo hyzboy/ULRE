7c689dc5e54ce619686c27586dfdcd74653418bd
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-09-17 23:21:49 +0800

    修正顶点数计算不正确问题

--- src/SceneGraph/InlineGeometry/Circle.cpp
+++ src/SceneGraph/InlineGeometry/Circle.cpp
@@ -11,13 +11,14 @@ namespace hgl::graph::inline_geometry
 
         if(cci->has_center)
         {
-            edge=cci->field_count+1;
-            vertex_count=cci->field_count+2;
+            edge = cci->field_count + 1;
+            vertex_count = cci->field_count + 2;
         }
         else
         {
-            edge=cci->field_count;
-            vertex_count=cci->field_count;
+            // Need one extra vertex to close the loop (duplicate of the first)
+            edge = cci->field_count + 1;
+            vertex_count = cci->field_count + 1;
         }
 
         if(!pc->Init("Circle",vertex_count,0))return(nullptr);
@@ -36,15 +37,15 @@ namespace hgl::graph::inline_geometry
                 color->Write(cci->center_color);
         }
 
-        for(uint i=0;i<edge;i++)
+        for(uint i = 0;i < edge;i++)
         {
-            float ang=float(i)/float(cci->field_count)*360.0f;
+            float ang = float(i) / float(cci->field_count) * 360.0f;
 
-            float x=cci->center.x+sin(deg2rad(ang))*cci->radius.x;
-            float y=cci->center.y+cos(deg2rad(ang))*cci->radius.y;
+            float x = cci->center.x + sin(deg2rad(ang)) * cci->radius.x;
+            float y = cci->center.y + cos(deg2rad(ang)) * cci->radius.y;
 
             vertex->Write(x,y);
-                
+
             if(color.IsValid())
                 color->Write(cci->border_color);
         }
@@ -61,18 +62,18 @@ namespace hgl::graph::inline_geometry
 
         if(cci->has_center)
         {
-            edge=cci->field_count+1;
-            vertex_count=cci->field_count+2;
+            edge = cci->field_count + 1;
+            vertex_count = cci->field_count + 2;
         }
         else
         {
-            edge=cci->field_count;
-            vertex_count=cci->field_count;
+            edge = cci->field_count;
+            vertex_count = cci->field_count;
         }
 
-        bool has_index=pc->hasIndex();
+        bool has_index = pc->hasIndex();
 
-        if(!pc->Init("Circle",vertex_count,has_index?vertex_count:0))return(nullptr);
+        if(!pc->Init("Circle",vertex_count,has_index ? vertex_count : 0))return(nullptr);
 
         VABMap3f vertex(pc->GetVABMap(VAN::Position));
         VABMap4f color(pc->GetVABMap(VAN::Color));
@@ -92,15 +93,15 @@ namespace hgl::graph::inline_geometry
                 normal->Write(AxisVector::Z);
         }
 
-        for(uint i=0;i<edge;i++)
+        for(uint i = 0;i < edge;i++)
         {
-            float ang=float(i)/float(cci->field_count)*360.0f;
+            float ang = float(i) / float(cci->field_count) * 360.0f;
 
-            float x=cci->center.x+sin(deg2rad(ang))*cci->radius.x;
-            float y=cci->center.y+cos(deg2rad(ang))*cci->radius.y;
+            float x = cci->center.x + sin(deg2rad(ang)) * cci->radius.x;
+            float y = cci->center.y + cos(deg2rad(ang)) * cci->radius.y;
 
             vertex->Write(x,y,0);
-                
+
             if(color.IsValid())
                 color->Write(cci->border_color);
 
@@ -110,11 +111,11 @@ namespace hgl::graph::inline_geometry
 
         if(has_index)
         {
-            IBMap *ib_map=pc->GetIBMap();
+            IBMap *ib_map = pc->GetIBMap();
 
-            if(pc->GetIndexType()==IndexType::U16)WriteIBO<uint16>((uint16 *)(ib_map->Map()),0,vertex_count);else
-            if(pc->GetIndexType()==IndexType::U32)WriteIBO<uint32>((uint32 *)(ib_map->Map()),0,vertex_count);else
-            if(pc->GetIndexType()==IndexType::U8 )WriteIBO<uint8 >((uint8  *)(ib_map->Map()),0,vertex_count);
+            if(pc->GetIndexType() == IndexType::U16)WriteIBO<uint16>((uint16 *)(ib_map->Map()),0,vertex_count);else
+                if(pc->GetIndexType() == IndexType::U32)WriteIBO<uint32>((uint32 *)(ib_map->Map()),0,vertex_count);else
+                    if(pc->GetIndexType() == IndexType::U8)WriteIBO<uint8 >((uint8 *)(ib_map->Map()),0,vertex_count);
 
             ib_map->Unmap();
         }
@@ -129,8 +130,8 @@ namespace hgl::graph::inline_geometry
         uint vertex_count;
         uint index_count;
 
-        vertex_count=cci->field_count;
-        index_count=(vertex_count-2)*3;
+        vertex_count = cci->field_count;
+        index_count = (vertex_count - 2) * 3;
 
         if(!pc->Init("Circle",vertex_count,index_count))return(nullptr);
 
@@ -141,15 +142,15 @@ namespace hgl::graph::inline_geometry
         if(!vertex.IsValid())
             return(nullptr);
 
-        for(uint i=0;i<cci->field_count+1;i++)
+        for(uint i = 0;i < cci->field_count + 1;i++)
         {
-            float ang=float(i)/float(cci->field_count)*360.0f;
+            float ang = float(i) / float(cci->field_count) * 360.0f;
 
-            float x=cci->center.x+sin(deg2rad(ang))*cci->radius.x;
-            float y=cci->center.y+cos(deg2rad(ang))*cci->radius.y;
+            float x = cci->center.x + sin(deg2rad(ang)) * cci->radius.x;
+            float y = cci->center.y + cos(deg2rad(ang)) * cci->radius.y;
 
             vertex->Write(x,y,0);
-                
+
             if(color.IsValid())
                 color->Write(cci->border_color);
 
@@ -158,11 +159,11 @@ namespace hgl::graph::inline_geometry
         }
 
         {
-            IBMap *ib_map=pc->GetIBMap();
+            IBMap *ib_map = pc->GetIBMap();
 
-            if(pc->GetIndexType()==IndexType::U16)WriteCircleIBO<uint16>((uint16 *)(ib_map->Map()),cci->field_count);else
-            if(pc->GetIndexType()==IndexType::U32)WriteCircleIBO<uint32>((uint32 *)(ib_map->Map()),cci->field_count);else
-            if(pc->GetIndexType()==IndexType::U8 )WriteCircleIBO<uint8 >((uint8  *)(ib_map->Map()),cci->field_count);
+            if(pc->GetIndexType() == IndexType::U16)WriteCircleIBO<uint16>((uint16 *)(ib_map->Map()),cci->field_count);else
+                if(pc->GetIndexType() == IndexType::U32)WriteCircleIBO<uint32>((uint32 *)(ib_map->Map()),cci->field_count);else
+                    if(pc->GetIndexType() == IndexType::U8)WriteCircleIBO<uint8 >((uint8 *)(ib_map->Map()),cci->field_count);
 
             ib_map->Unmap();
         }
