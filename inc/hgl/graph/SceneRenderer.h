664a21fb59adc2f2d0cf28a9b52f5052a0874de0
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-09-19 01:42:27 +0800

    纯排版

--- inc/hgl/graph/SceneRenderer.h
+++ inc/hgl/graph/SceneRenderer.h
@@ -18,34 +18,34 @@ namespace hgl::graph
     */
     class SceneRenderer
     {
-        IRenderTarget * render_target = nullptr;
-        Scene *         scene = nullptr;
+        IRenderTarget *     render_target       = nullptr;
+        Scene *             scene               = nullptr;
 
-        CameraControl * camera_control = nullptr;
+        CameraControl *     camera_control      = nullptr;
 
-        RenderTask *    render_task=nullptr;                                      ///<当前渲染任务
+        RenderTask *        render_task         = nullptr;                      ///<当前渲染任务
 
-        LineRenderManager * line_render_manager=nullptr;                         ///<线段渲染管理器(移出Scene)
+        LineRenderManager * line_render_manager = nullptr;                      ///<线段渲染管理器(移出Scene)
 
     protected:
 
-        Color4f clear_color;                                                     ///<清屏颜色
+        Color4f clear_color;                                                    ///<清屏颜色
 
         bool render_state_dirty=false;
 
     public:
 
-                RenderPass *GetRenderPass   (){return render_target->GetRenderPass();}      ///<取得当前渲染器RenderPass
+                RenderPass *        GetRenderPass       ()      {return render_target->GetRenderPass();}                ///<取得当前渲染器RenderPass
 
-        const   ViewportInfo *GetViewportInfo()const{return render_target->GetViewportInfo();}///<取得当前渲染器视口信息
+        const   ViewportInfo *      GetViewportInfo     ()const {return render_target->GetViewportInfo();}              ///<取得当前渲染器视口信息
 
-        const   VkExtent2D &GetExtent       ()const{return render_target->GetExtent();}     ///<取得当前渲染器画面尺寸
+        const   VkExtent2D &        GetExtent           ()const {return render_target->GetExtent();}                    ///<取得当前渲染器画面尺寸
 
-                Scene *     GetScene        ()const{return scene;}                          ///<获取场景世界
-                Camera *    GetCamera       ()const{return scene?scene->GetCamera():nullptr;}             ///<获取当前相机
-        const   CameraInfo *GetCameraInfo   ()const{return scene?scene->GetCameraInfo():nullptr;}         ///<获取当前相机信息
+                Scene *             GetScene            ()const {return scene;}                                         ///<获取场景世界
+                Camera *            GetCamera           ()const {return scene?scene->GetCamera():nullptr;}              ///<获取当前相机
+        const   CameraInfo *        GetCameraInfo       ()const {return scene?scene->GetCameraInfo():nullptr;}          ///<获取当前相机信息
 
-                LineRenderManager *GetLineRenderManager()const{ return line_render_manager; }   ///<取得线条渲染管理器
+                LineRenderManager * GetLineRenderManager()const {return line_render_manager;}                           ///<取得线条渲染管理器
 
     public:
 
