9466b43e241b8c26ee21e0a4e9e7d3723bf9b3cd
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-12-06 23:33:31 +0800

    删除子仓库CMPlatform

--- pull_all.bat
+++ pull_all.bat
@@ -6,6 +6,5 @@ git pull
 call pull.bat CMAssetsManage %REPO% %BRANCH%
 call pull.bat CMCMakeModule %REPO% %BRANCH%
 call pull.bat CMCore %REPO% %BRANCH%
-call pull.bat CMPlatform %REPO% %BRANCH%
 call pull.bat CMSceneGraph %REPO% %BRANCH%
 call pull.bat CMUtil %REPO% %BRANCH%
