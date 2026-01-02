371b2f08595a3a4d84aa2305b48c5c97194c2bdf
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-01-03 00:39:48 +0800

    增加Cube渲染测试，并修正Cube生成的三角形顶点顺序

---
+++ example/Basic/SimpleCube.cpp
@@ -0,0 +1,116 @@
+#include<hgl/WorkManager.h>
+#include<hgl/graph/geo/InlineGeometry.h>
+#include<hgl/graph/mtl/Material3DCreateConfig.h>
+#include<hgl/component/PrimitiveComponent.h>
+#include<hgl/color/Color.h>
+
+using namespace hgl;
+using namespace hgl::graph;
+
+class TestApp:public WorkObject
+{
+private:
+
+    Material *          material        = nullptr;
+    MaterialInstance *  mi              = nullptr;
+    Pipeline *          pipeline        = nullptr;
+
+    Geometry *          geometry        = nullptr;
+    Primitive *         primitive       = nullptr;
+
+private:
+
+    bool InitMaterial()
+    {
+        mtl::Material3DCreateConfig cfg(PrimitiveType::Triangles);
+
+        mtl::MaterialCreateInfo *mci = mtl::CreateGizmo3D(GetDevAttr(), &cfg);
+
+        if(!mci)
+            return false;
+
+        material = CreateMaterial("Gizmo3D", mci);
+
+        if(!material)
+            return false;
+
+        Color4f color = GetColor4f(COLOR::BlenderAxisBlue, 1.0f);
+
+        mi = CreateMaterialInstance(material, (VIL *)nullptr, &color);
+
+        if(!mi)
+            return false;
+
+        pipeline = CreatePipeline(material, InlinePipeline::Solid3D);
+
+        return pipeline != nullptr;
+    }
+
+    bool CreateCubeGeometry()
+    {
+        using namespace inline_geometry;
+
+        auto pc = GetGeometryCreater(material);
+
+        if(!pc)
+            return false;
+
+        CubeCreateInfo cci;
+        cci.segments_x = 1;
+        cci.segments_y = 1;
+        cci.segments_z = 1;
+
+        geometry = CreateCube(pc, &cci);
+
+        return geometry != nullptr;
+    }
+
+    bool InitScene()
+    {
+        primitive = CreatePrimitive(geometry, mi, pipeline);
+
+        if(!primitive)
+            return false;
+
+        CreateComponentInfo cci(GetWorldRootNode());
+
+        if(!CreateComponent<PrimitiveComponent>(&cci, primitive))
+            return false;
+
+        CameraControl *camera_control = GetCameraControl();
+
+        camera_control->SetPosition(math::Vector3f(2, 2, 2));
+        camera_control->SetTarget(math::Vector3f(0, 0, 0));
+
+        return true;
+    }
+
+public:
+
+    using WorkObject::WorkObject;
+
+    ~TestApp()
+    {
+        SAFE_CLEAR(primitive)
+        SAFE_CLEAR(geometry)
+    }
+
+    bool Init() override
+    {
+        if(!InitMaterial())
+            return false;
+
+        if(!CreateCubeGeometry())
+            return false;
+
+        if(!InitScene())
+            return false;
+
+        return true;
+    }
+};
+
+int os_main(int, os_char **)
+{
+    return RunFramework<TestApp>(OS_TEXT("Simple Cube"), 1280, 720);
+}
--- src/InlineGeometry/Cube.cpp
+++ src/InlineGeometry/Cube.cpp
@@ -5,6 +5,113 @@ namespace hgl::graph::inline_geometry
 {
     using namespace hgl::math;
 
+    namespace
+    {
+        inline uint CalcFaceVerts(uint seg_u, uint seg_v)
+        {
+            return (seg_u + 1) * (seg_v + 1);
+        }
+
+        inline uint CalcFaceIndices(uint seg_u, uint seg_v)
+        {
+            return seg_u * seg_v * 6;
+        }
+
+        inline void WriteIndex(uint idx, uint8_t*& idx8, uint16_t*& idx16, uint32_t*& idx32)
+        {
+            if(idx8)        *idx8++  = (uint8_t)idx;
+            else if(idx16)  *idx16++ = (uint16_t)idx;
+            else if(idx32)  *idx32++ = idx;
+        }
+
+        void GenerateFace(
+            const Vector3f& origin,
+            const Vector3f& u_axis,
+            const Vector3f& v_axis,
+            const Vector3f& normal,
+            const Vector3f& tangent,
+            uint segments_u,
+            uint segments_v,
+            GeometryBuilder& builder,
+            const CubeCreateInfo* cci,
+            uint& base_vertex,
+            uint8_t*& idx8,
+            uint16_t*& idx16,
+            uint32_t*& idx32)
+        {
+            // 计算u_axis和v_axis的叉积，判断是否与法线同向
+            // cross_product = u_axis × v_axis
+            float cross_x = u_axis.y * v_axis.z - u_axis.z * v_axis.y;
+            float cross_y = u_axis.z * v_axis.x - u_axis.x * v_axis.z;
+            float cross_z = u_axis.x * v_axis.y - u_axis.y * v_axis.x;
+            
+            // dot = cross_product · normal
+            float dot = cross_x * normal.x + cross_y * normal.y + cross_z * normal.z;
+            bool need_flip = (dot < 0.0f);
+            
+            // 生成顶点
+            for(uint v = 0; v <= segments_v; ++v)
+            {
+                float tv = float(v) / float(segments_v);
+                
+                for(uint u = 0; u <= segments_u; ++u)
+                {
+                    float tu = float(u) / float(segments_u);
+                    
+                    Vector3f pos = origin + u_axis * tu + v_axis * tv;
+                    
+                    builder.WriteVertex(pos.x, pos.y, pos.z);
+                    
+                    if(cci->normal)
+                        builder.WriteNormal(normal.x, normal.y, normal.z);
+                    
+                    if(cci->tangent)
+                        builder.WriteTangent(tangent.x, tangent.y, tangent.z);
+                    
+                    if(cci->tex_coord)
+                        builder.WriteTexCoord(tu, tv);
+                }
+            }
+            
+            // 生成索引 (根据面的朝向决定顶点顺序)
+            for(uint v = 0; v < segments_v; ++v)
+            {
+                for(uint u = 0; u < segments_u; ++u)
+                {
+                    uint i0 = base_vertex + v * (segments_u + 1) + u;
+                    uint i1 = i0 + 1;
+                    uint i2 = i0 + (segments_u + 1);
+                    uint i3 = i2 + 1;
+                    
+                    if(need_flip)
+                    {
+                        // 翻转顺序以保证CCW
+                        WriteIndex(i0, idx8, idx16, idx32);
+                        WriteIndex(i2, idx8, idx16, idx32);
+                        WriteIndex(i1, idx8, idx16, idx32);
+                        
+                        WriteIndex(i1, idx8, idx16, idx32);
+                        WriteIndex(i2, idx8, idx16, idx32);
+                        WriteIndex(i3, idx8, idx16, idx32);
+                    }
+                    else
+                    {
+                        // 正常CCW顺序
+                        WriteIndex(i0, idx8, idx16, idx32);
+                        WriteIndex(i1, idx8, idx16, idx32);
+                        WriteIndex(i2, idx8, idx16, idx32);
+                        
+                        WriteIndex(i1, idx8, idx16, idx32);
+                        WriteIndex(i3, idx8, idx16, idx32);
+                        WriteIndex(i2, idx8, idx16, idx32);
+                    }
+                }
+            }
+            
+            base_vertex += (segments_u + 1) * (segments_v + 1);
+        }
+    }
+
     Geometry *CreateCube(GeometryCreater *pc, const CubeCreateInfo *cci)
     {
         // 1. 参数验证
@@ -15,27 +122,20 @@ namespace hgl::graph::inline_geometry
             return nullptr;
 
         // 2. 计算顶点和索引数量
-        auto calc_face_verts = [](uint seg_u, uint seg_v) { 
-            return (seg_u + 1) * (seg_v + 1); 
-        };
-        auto calc_face_indices = [](uint seg_u, uint seg_v) { 
-            return seg_u * seg_v * 6; 
-        };
-        
         uint vertex_count = 0;
         uint index_count = 0;
         
         // 顶面和底面 (XZ平面)
-        vertex_count += calc_face_verts(cci->segments_x, cci->segments_z) * 2;
-        index_count += calc_face_indices(cci->segments_x, cci->segments_z) * 2;
+        vertex_count += CalcFaceVerts(cci->segments_x, cci->segments_z) * 2;
+        index_count += CalcFaceIndices(cci->segments_x, cci->segments_z) * 2;
         
         // 前面和后面 (XY平面)
-        vertex_count += calc_face_verts(cci->segments_x, cci->segments_y) * 2;
-        index_count += calc_face_indices(cci->segments_x, cci->segments_y) * 2;
+        vertex_count += CalcFaceVerts(cci->segments_x, cci->segments_y) * 2;
+        index_count += CalcFaceIndices(cci->segments_x, cci->segments_y) * 2;
         
         // 左面和右面 (ZY平面)
-        vertex_count += calc_face_verts(cci->segments_z, cci->segments_y) * 2;
-        index_count += calc_face_indices(cci->segments_z, cci->segments_y) * 2;
+        vertex_count += CalcFaceVerts(cci->segments_z, cci->segments_y) * 2;
+        index_count += CalcFaceIndices(cci->segments_z, cci->segments_y) * 2;
 
         // 3. 初始化 GeometryCreater
         if(!pc->Init("Cube", vertex_count, index_count))
@@ -66,129 +166,69 @@ namespace hgl::graph::inline_geometry
         else
             return nullptr;
 
-        // 辅助函数：写入索引
-        auto write_index = [&](uint idx) {
-            if(idx8)        *idx8++  = (uint8_t)idx;
-            else if(idx16)  *idx16++ = (uint16_t)idx;
-            else if(idx32)  *idx32++ = idx;
-        };
-
         // 6. 生成6个面的顶点和索引
         const float half = 0.5f;
         uint base_vertex = 0;
 
-        // 辅助函数：生成一个细分面
-        auto generate_face = [&](
-            const Vector3f& origin,
-            const Vector3f& u_axis,
-            const Vector3f& v_axis,
-            const Vector3f& normal,
-            const Vector3f& tangent,
-            uint segments_u,
-            uint segments_v)
-        {
-            // 生成顶点
-            for(uint v = 0; v <= segments_v; ++v)
-            {
-                float tv = float(v) / float(segments_v);
-                
-                for(uint u = 0; u <= segments_u; ++u)
-                {
-                    float tu = float(u) / float(segments_u);
-                    
-                    Vector3f pos = origin + u_axis * tu + v_axis * tv;
-                    
-                    builder.WriteVertex(pos.x, pos.y, pos.z);
-                    
-                    if(cci->normal)
-                        builder.WriteNormal(normal.x, normal.y, normal.z);
-                    
-                    if(cci->tangent)
-                        builder.WriteTangent(tangent.x, tangent.y, tangent.z);
-                    
-                    if(cci->tex_coord)
-                        builder.WriteTexCoord(tu, tv);
-                }
-            }
-            
-            // 生成索引
-            for(uint v = 0; v < segments_v; ++v)
-            {
-                for(uint u = 0; u < segments_u; ++u)
-                {
-                    uint i0 = base_vertex + v * (segments_u + 1) + u;
-                    uint i1 = i0 + 1;
-                    uint i2 = i0 + (segments_u + 1);
-                    uint i3 = i2 + 1;
-                    
-                    // 第一个三角形
-                    write_index(i0);
-                    write_index(i2);
-                    write_index(i1);
-                    
-                    // 第二个三角形
-                    write_index(i1);
-                    write_index(i2);
-                    write_index(i3);
-                }
-            }
-            
-            base_vertex += (segments_u + 1) * (segments_v + 1);
-        };
-
         // 底面 (Y-)
-        generate_face(
+        GenerateFace(
             Vector3f(-half, -half, -half),
             Vector3f(1.0f, 0.0f, 0.0f),
             Vector3f(0.0f, 0.0f, 1.0f),
             Vector3f(0.0f, -1.0f, 0.0f),
             Vector3f(1.0f, 0.0f, 0.0f),
-            cci->segments_x, cci->segments_z);
+            cci->segments_x, cci->segments_z,
+            builder, cci, base_vertex, idx8, idx16, idx32);
         
         // 顶面 (Y+)
-        generate_face(
+        GenerateFace(
             Vector3f(-half, half, half),
             Vector3f(1.0f, 0.0f, 0.0f),
             Vector3f(0.0f, 0.0f, -1.0f),
             Vector3f(0.0f, 1.0f, 0.0f),
             Vector3f(1.0f, 0.0f, 0.0f),
-            cci->segments_x, cci->segments_z);
+            cci->segments_x, cci->segments_z,
+            builder, cci, base_vertex, idx8, idx16, idx32);
         
         // 后面 (Z-)
-        generate_face(
+        GenerateFace(
             Vector3f(half, -half, -half),
             Vector3f(-1.0f, 0.0f, 0.0f),
             Vector3f(0.0f, 1.0f, 0.0f),
             Vector3f(0.0f, 0.0f, -1.0f),
             Vector3f(-1.0f, 0.0f, 0.0f),
-            cci->segments_x, cci->segments_y);
+            cci->segments_x, cci->segments_y,
+            builder, cci, base_vertex, idx8, idx16, idx32);
         
         // 前面 (Z+)
-        generate_face(
+        GenerateFace(
             Vector3f(-half, -half, half),
             Vector3f(1.0f, 0.0f, 0.0f),
             Vector3f(0.0f, 1.0f, 0.0f),
             Vector3f(0.0f, 0.0f, 1.0f),
             Vector3f(1.0f, 0.0f, 0.0f),
-            cci->segments_x, cci->segments_y);
+            cci->segments_x, cci->segments_y,
+            builder, cci, base_vertex, idx8, idx16, idx32);
         
         // 左面 (X-)
-        generate_face(
+        GenerateFace(
             Vector3f(-half, -half, half),
             Vector3f(0.0f, 0.0f, -1.0f),
             Vector3f(0.0f, 1.0f, 0.0f),
             Vector3f(-1.0f, 0.0f, 0.0f),
             Vector3f(0.0f, 0.0f, -1.0f),
-            cci->segments_z, cci->segments_y);
+            cci->segments_z, cci->segments_y,
+            builder, cci, base_vertex, idx8, idx16, idx32);
         
         // 右面 (X+)
-        generate_face(
+        GenerateFace(
             Vector3f(half, -half, -half),
             Vector3f(0.0f, 0.0f, 1.0f),
             Vector3f(0.0f, 1.0f, 0.0f),
             Vector3f(1.0f, 0.0f, 0.0f),
             Vector3f(0.0f, 0.0f, 1.0f),
-            cci->segments_z, cci->segments_y);
+            cci->segments_z, cci->segments_y,
+            builder, cci, base_vertex, idx8, idx16, idx32);
 
         // 7. Unmap索引缓冲区
         ib_map->Unmap();
