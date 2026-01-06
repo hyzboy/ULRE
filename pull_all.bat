ac363fbb9962b733ac379fa595a06653083f2716
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-01-07 00:50:47 +0800

    更新子仓库

--- pull_all.bat
+++ pull_all.bat
@@ -3,8 +3,10 @@ SET REPO="hyzgame"
 SET BRANCH=master
 
 git pull
+call pull.bat CM2D %REPO% %BRANCH%
 call pull.bat CMAssetsManage %REPO% %BRANCH%
 call pull.bat CMCMakeModule %REPO% %BRANCH%
 call pull.bat CMCore %REPO% %BRANCH%
-call pull.bat CMSceneGraph %REPO% %BRANCH%
+call pull.bat CMCoreType %REPO% %BRANCH%
+call pull.bat CMMath %REPO% %BRANCH%
 call pull.bat CMUtil %REPO% %BRANCH%
