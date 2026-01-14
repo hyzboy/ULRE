20ad0a224c3d08e513c8f1ab3e6c53cd9c0996a6
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-01-14 22:23:01 +0800

    增加InputSystem.h，删除错误的CameraComponent.cpp

---
+++ inc/hgl/ecs/InputSystem.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include<hgl/ecs/System.h>
+#include<hgl/io/event/WindowEvent.h>
+#include<hgl/math/Vector.h>
+
+namespace hgl::ecs
+{
+    using namespace hgl::math;
+
+    class InputSystem : public ecs::System,io::WindowEvent
+    {
+    protected:
+
+        Vector2i mouse_coord;
+
+        virtual io::EventProcResult OnEvent(const io::EventHeader &header, const uint64 data) override;
+
+    public:
+
+        const Vector2i &GetMouseCoord() const { return mouse_coord; }
+
+    public:
+
+        InputSystem() = default;
+        virtual ~InputSystem() = default;
+
+        void Update(float deltaTime) override;
+    };
+}//namespace hgl::ecs
