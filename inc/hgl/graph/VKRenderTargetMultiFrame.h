85f929cf72448b55e1ffe9c422e8de325a810c48
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-02-09 03:57:51 +0800

    ECSTransformAssignmentBuffer改用新的ringWriter

--- inc/hgl/graph/VKRenderTargetMultiFrame.h
+++ inc/hgl/graph/VKRenderTargetMultiFrame.h
@@ -49,7 +49,7 @@ public:
         return(true);
     }
 
-    uint32_t            GetCurrentFrameIndices      ()const{return current_frame;}
+    uint32_t            GetCurrentFrameIndex        ()const{return current_frame;}
     uint32_t            GetFrameCount               ()const{return frame_number;}
 
 public:
@@ -78,9 +78,6 @@ public: // Command Buffer
 
     bool                WaitQueue                   ()override{return rtd_list[current_frame].queue->WaitQueue();}
     bool                WaitFence                   ()override{return rtd_list[current_frame].queue->WaitFence();}
-
-    uint32_t            GetCurrentFrameIndex        ()const override{return current_frame;}
-    uint32_t            GetFrameCount               ()const override{return frame_number;}
 
 public:
 
