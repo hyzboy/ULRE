b2d664a2168af51c3056b1d67d81fbd7ed255eeb
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-02-08 15:57:36 +0800

    迁移RenderAssign,RenderTargetOutputConfig

--- inc/hgl/shader_schema/RenderTargetOutputConfig.h
+++ inc/hgl/shader_schema/RenderTargetOutputConfig.h
@@ -1,3 +1,16 @@
 #pragma once
 
-#include<hgl/graph/RenderTargetOutputConfig.h>
+#include<hgl/type/DataType.h>
+
+namespace hgl
+{
+	namespace graph
+	{
+		struct RenderTargetOutputConfig
+		{
+			uint color;                 ///<要输出几个颜色缓冲区
+			bool depth;                 ///<是否输出到深度缓冲区
+			bool stencil;               ///<是否输出到模板缓冲区
+		};
+	}//namespace graph
+}//namespace hgl
