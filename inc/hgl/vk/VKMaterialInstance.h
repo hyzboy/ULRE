c9bf22f4c7da74035fefa9fc52a02064ca00f333
hyzboy <hyzboy@outlook.com>
2026-03-19 18:33:11 +0800

    增加新的Material改造计划

--- inc/hgl/vk/VKMaterialInstance.h
+++ inc/hgl/vk/VKMaterialInstance.h
@@ -5,39 +5,6 @@
 namespace hgl::graph{
 
 /**
-* <summary>
-*
-*    layout(location=?) in uint MaterialInstanceID
-*
-*    #define MI_MAX_COUNT ???                //该值由引擎根据 UBORange/sizeof(MaterialInstance) 计算出来
-*
-*    struct MaterialInstance                 //这部分数据，即为材质实例的具体数据，每一个材质实例类负责提供具体数据。由RenderCollector合并成一整个UBO
-*    {                                       //该类数据，由DescriptorSetType为PerMaterial的参数构成
-*        vec4 BaseColor;
-*        vec4 Emissive;
-*        vec4 ARM;
-*    };
-*
-*    layout(set=?,binding=?) uniform Material
-*    {
-*        MaterialInstance mi[MI_MAX_COUNT]
-*    }mtl;
-*
-*    void main()
-*    {
-*        MaterialInstance mi=mtl.mi[(MaterialInstanceID>=MI_MAX_COUNT)?:0:MaterialInstanceID];   //如果超出范围则使用0号材质实例数据
-*
-*        vec4 BaseColor  =mi.BaseColor;
-*        vec4 Emissive   =mi.Emissive;
-*
-*        float AO        =mi.ARM.x;
-*        float Roughness =mi.ARM.y;
-*        float Metallic  =mi.ARM.z;
-*
-* </summary>
-*/
-
-/**
 * 材质实例类<br>
 * 材质实例类本质只是提供一个数据区，供RenderCollector合并成一个大UBO。
 */
