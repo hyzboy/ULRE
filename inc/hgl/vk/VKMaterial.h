0981b6fbd36501e5bdaf8ecccba67769311d808d
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-03-23 23:42:02 +0800

    标记旧函数为deprecated

--- inc/hgl/vk/VKMaterial.h
+++ inc/hgl/vk/VKMaterial.h
@@ -109,7 +109,9 @@ public:
         return BindTextureSampler(SET_TYPE_TEXTURE,slot,tex,sampler);
     }
 
+    [[deprecated("Use semantic-based overload instead")]]
     bool BindUBO(const DescriptorSetType &type,const AnsiString &name,const IGPUBuffer *gpu,bool dynamic=false);
+    [[deprecated("Use semantic-based overload instead")]]
     bool BindSSBO(const DescriptorSetType &type,const AnsiString &name,const IGPUBuffer *gpu,bool dynamic=false);
 
     bool BindUBO(const DescriptorSetType &type,const mtl::UBODescriptorSemantic semantic,const IGPUBuffer *gpu,bool dynamic=false);
