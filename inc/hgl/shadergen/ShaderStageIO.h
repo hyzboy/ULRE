463b3a338251caa1fd13c5e8412937729f5f264d
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-03-31 01:10:28 +0800

    删除ShaderStageIO中用不到的部分

--- inc/hgl/shadergen/ShaderStageIO.h
+++ inc/hgl/shadergen/ShaderStageIO.h
@@ -20,9 +20,7 @@ class ShaderStageIO
 protected:
 
     ShaderStage                         stage_flag;
-
     ConstValueDescriptorList            const_value_list;
-
     ShaderPushConstant                  push_constant;
 
 public:
@@ -58,49 +56,20 @@ public:
     VertexShaderStageIO() : ShaderStageIO(ShaderStage::Vertex) {}
     virtual ~VertexShaderStageIO() override = default;
 
-    bool AddInput(VIA &item)            { return input.Add(item); }
-    bool AddOutput(ShaderVariable &item){ return output.Add(item); }
-
-    bool hasInput(const VertexAttrib attrib) const { return input.Contains(attrib); }  ///<是否有指定输入
-    bool hasInput(const char *name) const
-    {
-        const VertexAttrib attrib = GetVertexAttribByName(name);
-        return (attrib != VertexAttrib::RANGE_SIZE) ? input.Contains(attrib) : false;
-    }
+    bool AddInput(VIA &item) { return input.Add(item); }
 
 public:
 
     VIAArray       &GetInput()       { return input; }
     const VIAArray &GetInput() const { return input; }
-    SVArray        &GetOutput()      { return output; }
-
-    bool IsEmptyInput()  const { return input.count == 0; }
-    bool IsEmptyOutput() const { return output.IsEmpty(); }
 };//class VertexShaderStageIO
 
 class FragmentShaderStageIO : public ShaderStageIO
 {
-    SVArray  input;
-    VIAArray output;
-
 public:
 
     FragmentShaderStageIO() : ShaderStageIO(ShaderStage::Fragment) {}
     virtual ~FragmentShaderStageIO() override = default;
-
-    bool AddInput(ShaderVariable &item){ return input.Add(item); }
-    bool AddOutput(VIA &item)          { return output.Add(item); }
-
-    bool hasInput(const char *name) const { return input.Contains(name); }  ///<是否有指定输入
-
-public:
-
-    SVArray        &GetInput()       { return input; }
-    const SVArray  &GetInput() const { return input; }
-    VIAArray       &GetOutput()      { return output; }
-
-    bool IsEmptyInput()  const { return input.IsEmpty(); }
-    bool IsEmptyOutput() const { return output.count == 0; }
 };//class FragmentShaderStageIO
 
 // Compute Shader 还没有启用，暂时注释掉
