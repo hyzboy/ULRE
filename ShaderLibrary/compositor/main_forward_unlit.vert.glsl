0209dd9a64d3b26e33b0a5e6f23166c88e8fe139
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-04-23 15:28:57 +0800

    删除旧shader

--- ShaderLibrary/compositor/main_forward_unlit.vert.glsl
+++
@@ -1,9 +0,0 @@
-#ifndef ULRE_COMPOSITOR_MAIN_FORWARD_UNLIT_VERT_GLSL
-#define ULRE_COMPOSITOR_MAIN_FORWARD_UNLIT_VERT_GLSL
-
-#version 450
-
-#include "compositor/vert_forward_ubo.glsl"
-#include "compositor/vert_forward_main.glsl"
-
-#endif // ULRE_COMPOSITOR_MAIN_FORWARD_UNLIT_VERT_GLSL
