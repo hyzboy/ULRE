0be22ac2af14e4fb07d06e52f93ccc08eed8c436
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-08-26 21:03:34 +0800

    增加竖排字符标记

--- src/SceneGraph/font/FontDataSource.cpp
+++ src/SceneGraph/font/FontDataSource.cpp
@@ -23,7 +23,7 @@ namespace hgl::graph
         constexpr u32char   BeginSymbols    []=U32_TEXT("!),❟.:;?]}¨·ˇˉ―‖’❜”„❞…∶、。〃々❯〉》」』】〕〗！＂＇），．：；？］｀｜｝～»›");  //行首禁用符号
         constexpr u32char   EndSymbols      []=U32_TEXT("([{·❛‘“‟❝❮〈《「『【〔〖（．［｛«‹");                                       //行尾禁用符号
         constexpr u32char   CurrencySymbols []=U32_TEXT("₳฿₿￠₡¢₢₵₫€￡£₤₣ƒ₲₭Ł₥₦₽₱＄$₮ℳ₶₩￦¥￥₴₸¤₰៛₪₯₠₧﷼㍐원৳₹₨৲௹");                //货币符号
-        constexpr u32char   VRotateSymbols  []=U32_TEXT("()[]{}〈〉《》「」『』【】〔〕〖〗（）［］｛｝―‖…∶｜～");                        //竖排必须旋转的符号
+        constexpr u32char   VRotateSymbols  []=U32_TEXT("()[]{}〈〉《》「」『』【】〔〕〖〗（）［］｛｝―‖…∶｜～\u2018\u2019\u201C\u201D\u201A\u201E\u2014\u2013\u2010\u2011\u2012\u2212\u00B7\u30FB$\u20AC\u00A5\u00A3\u00A2\u20B9=+\u00B1\u00D7\u00F7<>\u2264\u2265%\u2030#&@\u2190\u2192");                        //竖排必须旋转的符号
 
         constexpr int       BeginSymbolsCount   =(sizeof(BeginSymbols)   /sizeof(u32char))-1;
         constexpr int       EndSymbolsCount     =(sizeof(EndSymbols)     /sizeof(u32char))-1;
@@ -60,7 +60,10 @@ namespace hgl::graph
                 if(!attr->end_disable)
                 attr->end_disable   =attr->is_currency;     //货币符号同样行尾禁用
 
-                attr->vrotate       =hgl::strchr(VRotateSymbols,    ch,VRotateSymbolsCount  );
+                attr->vrotate       =hgl::strchr(VRotateSymbols,    ch,VRotateSymbolsCount  )
+                                    ||hgl::isLatin(ch)||hgl::isCyrillic(ch)||hgl::isGreek(ch)
+                                    ||hgl::isArabic(ch)||hgl::isHebrew(ch)
+                                    ||hgl::isThai(ch)||hgl::isDevanagari(ch);
 
                 attr->is_cjk        =isCJK(ch);
                 attr->is_emoji      =isEmoji(ch);
